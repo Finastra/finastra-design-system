dd8250c814116dfa4f39d2bc86daf11e8d013023
Alexandru Ifrim <alexandru.ifrim@gmail.com>
2019-07-18 17:09:15 +0300

    rename app & components

--- libs/angular-components/jest.config.js
+++ libs/angular-components/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'components',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/components',
+  coverageDirectory: '../../coverage/libs/angular-components',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js'
