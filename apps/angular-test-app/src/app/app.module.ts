abc3081f1aba5d8a644142ff7df5679ad4ef1871
Alexandru Ifrim <alexandru.ifrim@gmail.com>
2019-07-30 16:43:32 +0300

    use scroll-to-top in angular test app

--- apps/angular-test-app/src/app/app.module.ts
+++ apps/angular-test-app/src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScrollToTopModule } from '@ffdc/angular-components/scroll-to-top';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ScrollToTopModule,
+    MatIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
