61138b18153ad3b80de778775d86e4e8a0724617
David <bcldvd@gmail.com>
2019-08-08 15:24:08 +0200

    test(demo-app): fix tests

--- apps/angular-test-app/src/app/components/global-search-demo/global-search-demo.component.spec.ts
+++ apps/angular-test-app/src/app/components/global-search-demo/global-search-demo.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GlobalSearchDemoComponent } from './global-search-demo.component';
+import { GlobalSearchModule } from '@ffdc/uxg-angular-components/global-search';
 
 describe('GlobalSearchDemoComponent', () => {
   let component: GlobalSearchDemoComponent;
@@ -8,6 +9,7 @@ describe('GlobalSearchDemoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ GlobalSearchModule ],
       declarations: [ GlobalSearchDemoComponent ]
     })
     .compileComponents();
--- apps/angular-test-app/src/app/components/home/home.component.spec.ts
+++ apps/angular-test-app/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { MatCardModule, MatChipsModule } from '@angular/material';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,6 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ MatCardModule, MatChipsModule ],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
