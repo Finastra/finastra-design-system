9d4bcb2f5a1d603196c04af8389e1b7b484a8cf2
pingyuan <ping.yuan@finastra.com>
2019-08-28 13:34:24 +0200

    table fine tunning

--- apps/angular-test-app/src/app/components/table-demo/table-demo.component.ts
+++ apps/angular-test-app/src/app/components/table-demo/table-demo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
-import { Sort } from '@angular/material';
+import { Sort, PageEvent } from '@angular/material';
 
 const ELEMENT_DATA: any[] = [
   {'API': 'Exchange Rates', 'End Point': 'End point 1', 'Date Time': '1-1-2019', 'Day of Week': 'Monday', 'Hour of Day': '16h-17h', 'Status Code': "200", 'Error Response': "OK", "No. of Calls": 3},
@@ -27,6 +27,7 @@ const ELEMENT_DATA: any[] = [
 export class TableDemoComponent implements OnInit {
 
   dataSource = ELEMENT_DATA;  //data
+  length  = ELEMENT_DATA.length;
 
   // columns defination object
   columns: any[] = [
@@ -44,12 +45,13 @@ export class TableDemoComponent implements OnInit {
                       'Hour of Day', 'Status Code',
                       'Error Response', 'No. of Calls'];
 
-  constructor(){}
+  constructor() {}
 
   ngOnInit(): void {
+    this.applyDefaultPaging();
   }
 
-  sortData(sort: Sort){
+  sortData(sort: Sort) {
     this.dataSource = this.dataSource.slice().sort( (obj1, obj2) => {
       switch(this.getSortColumnType(sort.active)){
         case 'number':
@@ -77,10 +79,21 @@ export class TableDemoComponent implements OnInit {
       }
     })
   }
-  getSortColumnType(columnName: string){
+  getSortColumnType(columnName: string) {
     return this.columns.find( column => { 
       return column.name === columnName}
       ).type;
   }
-
+  applyPaging($event: PageEvent) {
+    const offset = $event.pageSize * $event.pageIndex;
+    this.dataSource = ELEMENT_DATA.slice(offset, offset + $event.pageSize);
+  }
+  applyDefaultPaging() {
+    const defaultPaging: PageEvent = {
+      length: this.length,
+      pageIndex: 0,
+      pageSize: 5
+    }
+    this.applyPaging(defaultPaging);
+  }
 }
