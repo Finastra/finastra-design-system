43c47887e65f3e51b97eac079a893e20bd04abc5
Razvan Bordeanu <razvan.bordeanu@finastra.com>
2019-09-10 14:05:09 +0300

    code review + show better crumbs

--- apps/angular-test-app/src/app/routes.ts
+++ apps/angular-test-app/src/app/routes.ts
@@ -6,7 +6,7 @@ import { TableDemoComponent } from './components/table-demo/table-demo.component
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'global-search', component: GlobalSearchDemoComponent },
-  { path: 'table', component: TableDemoComponent },
-  { path: 'popover', component: PopoverDemoComponent }
+  { path: 'global-search', component: GlobalSearchDemoComponent, data: { title: 'Global Search Demo' } },
+  { path: 'table', component: TableDemoComponent, data: { title: 'Table Demo' } },
+  { path: 'popover', component: PopoverDemoComponent, data: { title: 'Popover Demo' } }
 ];
