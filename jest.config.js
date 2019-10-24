2ebb1e165bfcc480faa39b395323900ca5e464fb
Pawel Leszczewicz <pawel.leszczewicz@finastra.com>
2019-10-24 07:29:16 +0200

    added coverage collection and printing joined report

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,38 @@
+const esModules = ['lodash-es', '@ngrx'].join('|');
+const babelModules = ['lodash-es'].join('|');
+
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
+  verbose: true,
+  testMatch: ['**/+(*.)+(spec|test).+ts'],
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
-  reporters: ['default', 'jest-junit']
-};
+  preset: "jest-preset-angular",
+  resolver: '@nrwl/jest/plugins/resolver',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+    [`(${babelModules}).+\\.js$`]: 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    `/node_modules/(?!${esModules})`
+  ],
+  testEnvironment: 'jest-environment-jsdom-thirteen',
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: './testresults/', outputName: `junit-${new Date().getTime()}.xml` }]
+  ],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.spec.json'
+    }
+  },
+  // Coverage
+  collectCoverage: true,
+  coverageReporters: ['json', "html"],
+  coverageThreshold: {
+    // global: {
+    //   branches: 60,
+    //   functions: 80,
+    //   lines: 80,
+    //   statements: 80
+    // }
+  }
+}
