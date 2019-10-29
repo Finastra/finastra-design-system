8b5b55b6c9b9a9c34a4a7111cffac7fcc8dbd01f
Pawel Leszczewicz <pawel.leszczewicz@finastra.com>
2019-10-29 14:40:21 +0100

    improve coverage, reduce unnecessary method

--- libs/angular-components/scroll-to-top/src/scroll-to-top.component.spec.ts
+++ libs/angular-components/scroll-to-top/src/scroll-to-top.component.spec.ts
@@ -1,26 +1,41 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatIconModule } from '@angular/material';
 import { ScrollToTopComponent } from './scroll-to-top.component';
+import { By } from '@angular/platform-browser';
 
 describe('ScrollToTopComponent', () => {
   let component: ScrollToTopComponent;
   let fixture: ComponentFixture<ScrollToTopComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [MatButtonModule, MatIconModule, BrowserAnimationsModule],
       declarations: [ScrollToTopComponent]
     }).compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(ScrollToTopComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create component instance', () => {
+    expect(component).toBeDefined();
   });
+
+  it('should trigger onClick when button is clicked', () => {
+    window.scroll = jest.fn();
+    const button = fixture.debugElement.query(By.css('button'));
+
+    button.nativeElement.click();
+    fixture.detectChanges();
+
+    expect(component.parent.scroll).toBeCalledWith({ top: 0, left: 0, behavior: 'smooth' });
+  })
+
+  it('should be able to setup different parent than window', () => {
+    component.parentElementSelector = 'mockedParent';
+    component.ngAfterViewInit();
+
+    expect(component.parent).not.toBe(window);
+  })
 });
