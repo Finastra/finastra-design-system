e8eefdaf6b64eee7e8e96f9c46e66aec8e4e0ac0
Razvan Bordeanu <razvan.bordeanu@gmail.com>
2019-12-10 19:20:39 +0200

    fix test

--- libs/angular-components/wizard/src/services/button-hub.service.spec.ts
+++ libs/angular-components/wizard/src/services/button-hub.service.spec.ts
@@ -3,7 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { ButtonHubService } from './button-hub.service';
 
 describe('ButtonHubService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      ButtonHubService
+    ]
+  }));
 
   it('should be created', () => {
     const service: ButtonHubService = TestBed.get(ButtonHubService);
