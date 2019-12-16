7f38be90baf54da916e3bdac4433eaf7d12d21b4
pingyuan <jenkins@misys.com>
2019-12-16 17:32:21 +0100

    import lodash-es

--- libs/angular-components/table/src/table.component.ts
+++ libs/angular-components/table/src/table.component.ts
@@ -14,7 +14,7 @@ import { MatTable, PageEvent, MatCheckbox } from '@angular/material';
 
 import { UxgColumn, UxgSort, UxgPage, UxgColumnType, UxgTableSelectEvent, UxgDefaultPaging } from './table.models';
 import { CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
-import * as isEqual from 'lodash/isEqual';
+import * as isEqual from 'lodash-es/isEqual';
 @Component({
   selector: 'uxg-table',
   templateUrl: './table.component.html',
