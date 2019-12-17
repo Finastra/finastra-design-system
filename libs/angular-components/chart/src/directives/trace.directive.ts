e9dc507172c77d70877047fed1ce7c18e3cadd26
Coulaud, Romuald <romuald.coulaud@gmail.com>
2019-12-17 17:29:02 +0100

    Add set selectionpoints and horizontal chart

--- libs/angular-components/chart/src/directives/trace.directive.ts
+++ libs/angular-components/chart/src/directives/trace.directive.ts
@@ -8,24 +8,34 @@ export class Trace {
     @Input() dimensionName: string;
     @Input() measure: Array<string | number>;
     @Input() measureName: string;
-    @Input() selectedPoints: Array<any>;
+    @Input() selectedPoints: Array<number>;
     @Input() type: ChartType;
-    @Input() orientation: string;
+    @Input() orientation: 'horizontal' | 'vertical';
     @Input() options: Object;
+
+    constructor() {
+        this.orientation = 'vertical';
+    }
 
     getPlotlyTrace(): Partial<Plotly.Data> {
         const plotlyType = chartTypePlotly[this.type];
-        if (plotlyType)
-            return {
+        if (plotlyType) {
+            const trace = {
                 dimensionName: this.dimensionName,
-                x: this.dimension,
-                y: this.measure,
-                values: this.measure,
-                labels: this.dimension,
+                x: this.orientation === 'vertical' ? this.dimension : this.measure,
+                y: this.orientation === 'vertical' ? this.measure : this.dimension,
+                labels: this.orientation === 'vertical' ? this.dimension : this.measure,
+                values: this.orientation === 'vertical' ? this.measure : this.dimension,
                 name: this.measureName,
                 ...plotlyType ? plotlyType.trace : {},
+                orientation: this.orientation === 'horizontal' ? 'h' : 'v',
                 ...this.options ? this.options : {}
             }
+            if (this.selectedPoints) {
+                trace.selectedpoints = this.selectedPoints;
+            }
+            return trace;
+        }
         console.error("Chart type: " + this.type + " doesn't exist!");
         return undefined;
     }
