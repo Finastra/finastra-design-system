4d1a05e90dbe36596276d2e93e477b46b37c90d9
Razvan Bordeanu <razvan.bordeanu@gmail.com>
2020-02-11 02:09:46 +0200

    fix

--- libs/angular-components/wizard/index.ts
+++ libs/angular-components/wizard/index.ts
@@ -1,3 +1,7 @@
 export * from './src/wizard.module';
 export * from './src/wizard.component';
 export * from './src/wizard-title.component';
+export * from './src/wizard-button/wizard-button.component';
+export * from './src/wizard-page/wizard-page.component';
+export * from './src/wizard-page/wizard-page-title.directive';
+export * from './src/wizard-page/wizard-page-description.directive';
