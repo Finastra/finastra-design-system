597c75a3c337640bc81da2b86d5b7a9b90947af7
Thomas TALBOT <thomas.talbot@gmail.com>
2020-02-12 10:06:28 +0100

    feat(app): add theme in test app.

---
+++ apps/angular-test-app/src/app/components/ui-elements-demo/ui-elements-demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-elements-demo',
+  templateUrl: './ui-elements-demo.component.html',
+  styleUrls: ['./ui-elements-demo.component.scss']
+})
+export class UiElementsDemoComponent {
+  constructor() {}
+}
