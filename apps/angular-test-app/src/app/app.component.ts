80bb40a197fdfa9e5dc88d2aac440398325b7abf
Thomas TALBOT <thomas.talbot@gmail.com>
2020-02-13 14:32:09 +0100

    feat(app): enable dark theme in test app

--- apps/angular-test-app/src/app/app.component.ts
+++ apps/angular-test-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'ffdc-root',
@@ -8,12 +9,27 @@ import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 })
 export class AppComponent {
   title = '';
+  dark = false;
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  constructor(private router: Router, private route: ActivatedRoute, private _element: ElementRef<HTMLElement>, private _overlayContainer: OverlayContainer) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.title = this.route.root.firstChild ? this.route.root.firstChild.snapshot.data['title'] : '';
       }
     });
+  }
+
+  toggleTheme() {
+    const darkThemeClass = 'uxg-dark-theme';
+
+    this.dark = !this.dark;
+
+    if (this.dark) {
+      this._element.nativeElement.classList.add(darkThemeClass);
+      this._overlayContainer.getContainerElement().classList.add(darkThemeClass);
+    } else {
+      this._element.nativeElement.classList.remove(darkThemeClass);
+      this._overlayContainer.getContainerElement().classList.remove(darkThemeClass);
+    }
   }
 }
