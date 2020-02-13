51b7a85ad74c9170ce71636213ee6dc0cf3e212e
Thomas TALBOT <thomas.talbot@gmail.com>
2020-02-13 17:33:12 +0100

    feat(app): enable dark theme in test app

--- apps/angular-test-app/src/app/app.component.spec.ts
+++ apps/angular-test-app/src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -16,6 +17,7 @@ describe('AppComponent', () => {
       imports: [
         MatCardModule,
         MatSidenavModule,
+        MatSlideToggleModule,
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
--- apps/angular-test-app/src/app/app.component.ts
+++ apps/angular-test-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'ffdc-root',
@@ -8,12 +10,33 @@ import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 })
 export class AppComponent {
   title = '';
+  dark = false;
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    private _overlayContainer: OverlayContainer
+  ) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.title = this.route.root.firstChild ? this.route.root.firstChild.snapshot.data['title'] : '';
       }
     });
+  }
+
+  toggleTheme() {
+    const darkThemeClass = 'uxg-dark-theme';
+
+    this.dark = !this.dark;
+
+    if (this.dark) {
+      this.renderer.addClass(this.document.body, darkThemeClass);
+      this._overlayContainer.getContainerElement().classList.add(darkThemeClass);
+    } else {
+      this.renderer.removeClass(this.document.body, darkThemeClass);
+      this._overlayContainer.getContainerElement().classList.remove(darkThemeClass);
+    }
   }
 }
