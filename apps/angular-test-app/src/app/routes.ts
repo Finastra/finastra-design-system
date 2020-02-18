851b6e87f71d7797187b4d19bdb8d86d109a3d0a
Coulaud, Romuald <romuald.coulaud@gmail.com>
2020-02-18 11:23:51 +0100

    fix conflict

--- apps/angular-test-app/src/app/routes.ts
+++ apps/angular-test-app/src/app/routes.ts
@@ -1,26 +1,8 @@
 import { Routes } from '@angular/router';
-import { FilterPanelDemoComponent } from './components/filter-panel-demo/filter-panel-demo.component';
-import { GlobalSearchDemoComponent } from './components/global-search-demo/global-search-demo.component';
 import { HomeComponent } from './components/home/home.component';
-import { PopoverDemoComponent } from './components/popover-demo/popover-demo.component';
-import { RepeaterDemoComponent } from './components/repeater-demo/repeater-demo.component';
-import { TableDemoComponent } from './components/table-demo/table-demo.component';
-import { VectorMapDemoComponent } from './components/vector-map-demo/vector-map-demo.component';
-import { ChartDemoComponent } from './components/chart-demo/chart-demo.component';
-import { WizardDemoComponent } from './components/wizard-demo/wizard-demo.component';
-import { FoundationsDemoComponent } from './components/foundations-demo/foundations-demo.component';
-import { UiElementsDemoComponent } from './components/ui-elements-demo/ui-elements-demo.component';
+
+import { nestedRoutes } from './nested-routes';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'chart', component: ChartDemoComponent, data: { title: 'Chart' } },
-  { path: 'global-search', component: GlobalSearchDemoComponent, data: { title: 'Global Search Demo' } },
-  { path: 'table', component: TableDemoComponent, data: { title: 'Table Demo' } },
-  { path: 'popover', component: PopoverDemoComponent, data: { title: 'Popover Demo' } },
-  { path: 'repeater', component: RepeaterDemoComponent },
-  { path: 'filter-panel', component: FilterPanelDemoComponent, data: { title: 'Filter Demo' } },
-  { path: 'vector-map', component: VectorMapDemoComponent, data: { title: 'Vector Map' } },
-  { path: 'wizard', component: WizardDemoComponent, data: { title: 'Wizard' } },
-  { path: 'foundations', component: FoundationsDemoComponent, data: { title: 'Foundations' } },
-  { path: 'ui-elements', component: UiElementsDemoComponent, data: { title: 'UI Elements' } }
-];
+  ...([] as Routes).concat(...nestedRoutes.map(nestedRoute => nestedRoute.routes))
