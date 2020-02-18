519f100127176c0657f98e36782835d67b98eccd
Nguyen Thai Vinh <code2nguyen@gmail.com>
2020-02-18 13:05:32 +0100

    fix: remove @ffdc/uxg-angular-components  entrypoint

--- libs/angular-components/index.ts
+++ libs/angular-components/index.ts
@@ -1,10 +1,1 @@
-export * from './core';
-export * from './global-search';
-export * from './popover';
-export * from './scroll-to-top';
-export * from './table';
-export * from './filter/filter-tree';
-export * from './filter/filter-tags';
-export * from './vector-map';
-export * from './repeater';
-export * from './wizard';
+export default {}
