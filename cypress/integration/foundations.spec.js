95c2393a0157edc26156a64799aa970cb5931d47
Thomas TALBOT <thomas.talbot@gmail.com>
2020-03-16 14:35:08 +0100

    feat(tests): add visual tests with cypress snapshots

---
+++ cypress/integration/foundations.spec.js
@@ -0,0 +1,46 @@
+
+
+context('Foundations', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200')
+    cy.contains('menu').click()
+    cy.contains('Foundations').click()
+  })
+
+  it('match icons colors', () => {
+    cy.get('.icon-colors-test')
+      .scrollIntoView({offset: {top: "-50px"}})
+      .toMatchImageSnapshot()
+  })
+  
+  it('match icons sizes', () => {
+    cy.get('.icon-sizes-test')
+      .scrollIntoView({offset: {top: "-50px"}})
+      .toMatchImageSnapshot()
+  })
+
+  it('match icon types', () => {
+    cy.get('.icon-types-test')
+      .scrollIntoView({offset: {top: "-50px"}})
+      .toMatchImageSnapshot()
+  })
+
+  it('match logo nomal', () => {
+    cy.get('.logo-normal-test')
+      .scrollIntoView({offset: {top: "-50px"}})
+      .toMatchImageSnapshot()
+  })
+
+  it('match logo dense', () => {
+    cy.get('.logo-dense-test')
+      .scrollIntoView({offset: {top: "-50px"}})
+      .toMatchImageSnapshot()
+  })
+
+  it('match logo link', () => {
+    cy.get('.logo-link-test')
+      .scrollIntoView({offset: {top: "-50px"}})
+      .toMatchImageSnapshot()
+  })
+
+})
