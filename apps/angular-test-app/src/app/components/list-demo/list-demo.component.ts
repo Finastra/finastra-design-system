672a9ac95b3b5cda48801afdd97b78b58b91ba1d
dorsaffrigui <dorsaf.frigui@finastra.com>
2020-03-24 18:43:54 +0100

    list with inputs

--- apps/angular-test-app/src/app/components/list-demo/list-demo.component.ts
+++ apps/angular-test-app/src/app/components/list-demo/list-demo.component.ts
@@ -6,4 +6,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-demo.component.scss']
 })
 export class ListDemoComponent {
+  title ="products"
+  columnsMatcher ="name"
+  length =2;
+
+  countries: any[] = [
+    {
+      name: 'Dubai',
+      country: 'United Arab Emirates',
+    },
+    {
+      name: 'London',
+      country: 'United Kingdom',
+    },
+    {
+      name: 'New York',
+      country: 'United States of America',
+    },
+    {
+      name: 'Paris',
+      country: 'France',
+    },
+    {
+      name: 'Singapore',
+      country: 'Republic of Singapore',
+    },
+    {
+      name: 'Sydney',
+      country: 'Australia',
+    },
+    {
+      name: 'Toronto',
+      country: 'Canada',
+    }
+  ];
+
+  products = [
+    {
+      productId: "Finances",
+      name: "Finances",
+      homeUrl: "https://res.cloudinary.com/dmv2klkv7/image/upload/c_scale,w_1300/v1567769894/samples-powerbi_xdk2vt.png",
+      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
+      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
+      applications: [
+      ]
+    },
+    {
+      productId: "Human Resources",
+      name: "Human Resources",
+      homeUrl: "https://app.powerbi.com/reportEmbed?reportId=04c1a304-e640-4d1c-adbf-e468b661b362&autoAuth=true&ctid=0b9b90da-3fe1-457a-b340-f1b67e1024fb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0In0%3D",
+      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
+      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
+      applications: []
+    },
+    {
+      productId: "Finances",
+      name: "Finances",
+      homeUrl: "https://res.cloudinary.com/dmv2klkv7/image/upload/c_scale,w_1300/v1567769894/samples-powerbi_xdk2vt.png",
+      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
+      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
+      applications: [
+      ]
+    },
+    {
+      productId: "Human Resources",
+      name: "Human Resources",
+      homeUrl: "https://app.powerbi.com/reportEmbed?reportId=04c1a304-e640-4d1c-adbf-e468b661b362&autoAuth=true&ctid=0b9b90da-3fe1-457a-b340-f1b67e1024fb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0In0%3D",
+      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
+      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
+      applications: []
+    } 
+  ];
 }
--- libs/angular-components/list/src/list.component.ts
+++ libs/angular-components/list/src/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, Input } from '@angular/core';
 
 @Component({
   selector: 'uxg-list',
@@ -9,55 +9,18 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 
 export class ListComponent {
-  
-   products = [
-    {
-      productId: "Finances",
-      name: "Finances",
-      homeUrl: "https://res.cloudinary.com/dmv2klkv7/image/upload/c_scale,w_1300/v1567769894/samples-powerbi_xdk2vt.png",
-      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
-      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
-      applications: [
-        {
-          appId: "Customer Profitability",
-          name: "Customer Profitability",
-          description: "velit eu est congue elementum in hac habitasse platea dictumst morbi vestibulum velit id pretium iaculis diam erat fermentum justo",
-          url: "https://app.powerbi.com/reportEmbed?reportId=ab5a99e3-17a9-4f5b-80e5-6e86bab9427e&autoAuth=true&ctid=0b9b90da-3fe1-457a-b340-f1b67e1024fb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0In0%3D",
-          version: "1.1"
-        }
-      ]
-    },
-    {
-      productId: "Human Resources",
-      name: "Human Resources",
-      homeUrl: "https://app.powerbi.com/reportEmbed?reportId=04c1a304-e640-4d1c-adbf-e468b661b362&autoAuth=true&ctid=0b9b90da-3fe1-457a-b340-f1b67e1024fb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0In0%3D",
-      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
-      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
-      applications: []
-    },
-    {
-      productId: "Human Resources",
-      name: "Human Resources",
-      homeUrl: "https://app.powerbi.com/reportEmbed?reportId=04c1a304-e640-4d1c-adbf-e468b661b362&autoAuth=true&ctid=0b9b90da-3fe1-457a-b340-f1b67e1024fb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0In0%3D",
-      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
-      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
-      applications: []
-    },
-    {
-      productId: "Human Resources",
-      name: "Human Resources",
-      homeUrl: "https://app.powerbi.com/reportEmbed?reportId=04c1a304-e640-4d1c-adbf-e468b661b362&autoAuth=true&ctid=0b9b90da-3fe1-457a-b340-f1b67e1024fb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0In0%3D",
-      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
-      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
-      applications: []
-    },
-    {
-      productId: "Human Resources",
-      name: "Human Resources",
-      homeUrl: "https://app.powerbi.com/reportEmbed?reportId=04c1a304-e640-4d1c-adbf-e468b661b362&autoAuth=true&ctid=0b9b90da-3fe1-457a-b340-f1b67e1024fb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0In0%3D",
-      description: "quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
-      image: "http://dummyimage.com/239x174.jpg/5fa2dd/ffffff",
-      applications: []
+
+  @Input() title ='';
+  @Input() columnsMatcher =''
+  @Input() abbreviationLength =0
+  @Input() data: Array<any> =[];
+ 
+
+  formatItemName(name:string) {
+    if (name) {
+      return name.split(' ').map(str => str.charAt(0)).join('').toUpperCase().substring(0, this.abbreviationLength ? this.abbreviationLength : 1);
     }
-  ];
+    return name;
+  }
+  
 }
