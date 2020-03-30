fdc831b852b79ca2c3332e4278fe86586fccb7bd
David <david.bocle@finastra.com>
2020-03-30 15:42:21 +0200

    build(entity-menu): fix unit tests

--- libs/angular-components/entity-menu/src/entity-menu.component.spec.ts
+++ libs/angular-components/entity-menu/src/entity-menu.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EntityMenuComponent } from './entity-menu.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 describe('EntityMenuModule', () => {
   let component: EntityMenuComponent;
@@ -8,6 +12,7 @@ describe('EntityMenuModule', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MatButtonModule, MatIconModule, MatCardModule, MatMenuModule],
       declarations: [EntityMenuComponent]
     }).compileComponents();
   }));
