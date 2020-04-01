194b6a4c512305c1a8f55f2b823116fe9946a40f
pingyuan <jenkins@misys.com>
2020-04-01 16:38:10 +0200

    fix table header align

--- apps/angular-test-app/src/app/components/table-demo/table-demo.component.ts
+++ apps/angular-test-app/src/app/components/table-demo/table-demo.component.ts
@@ -254,7 +254,7 @@ export class TableDemoComponent implements OnInit {
     });
 
     this.columns = [
-      { name: 'API', type: UxgColumnType.string, align: 'left', displayName: 'Display Api' },
+      { name: 'API', type: UxgColumnType.string, align: 'center', displayName: 'Display Api' },
       { name: 'End Point', type: UxgColumnType.string, align: 'left' },
       { name: 'Date Time', type: UxgColumnType.date, align: 'left' },
       { name: 'Day of Week', type: UxgColumnType.string, align: 'left' },
