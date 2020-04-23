0ffc54b67fdc1d1df97fd01b1ad43df67a89e752
Thomas TALBOT <thomas.talbot@gmail.com>
2020-04-23 10:54:36 +0200

    fix(format): fix linting and formating

--- cypress/integration/foundations.spec.js
+++ cypress/integration/foundations.spec.js
@@ -1,12 +1,10 @@
-
-
 context('Foundations', () => {
   beforeEach(() => {
     cy.visit('/foundations');
     cy.get('.app-content').invoke('css', 'position', 'relative');
     //cy.contains('menu').click();
     //cy.contains('Foundations').click();
-  })
+  });
 
   ////////
   // ICONS
@@ -16,19 +14,19 @@ context('Foundations', () => {
     cy.get('.icon-colors-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
-  
+  });
+
   it('match icons sizes', () => {
     cy.get('.icon-sizes-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match icon types', () => {
     cy.get('.icon-types-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   ////////
   // LOGO
@@ -38,19 +36,19 @@ context('Foundations', () => {
     cy.get('.logo-normal-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match logo dense', () => {
     cy.get('.logo-dense-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match logo link', () => {
     cy.get('.logo-link-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   ////////
   // TYPO
@@ -60,29 +58,29 @@ context('Foundations', () => {
     cy.get('.typo-display-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match headline scale', () => {
     cy.get('.typo-headline-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match subtitle scale', () => {
     cy.get('.typo-subtitle-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match body scale', () => {
     cy.get('.typo-body-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match small scale', () => {
     cy.get('.typo-small-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
-})
+  });
+});
--- cypress/integration/ui-elements.spec.js
+++ cypress/integration/ui-elements.spec.js
@@ -4,8 +4,8 @@ context('UI Elements', () => {
     cy.get('.app-content').invoke('css', 'position', 'relative');
     //cy.contains('menu').click();
     //cy.contains('UI Elements').click();
-  })
-  
+  });
+
   ////////
   // AVATAR
   ////////
@@ -14,31 +14,31 @@ context('UI Elements', () => {
     cy.get('.avatar-default-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match letter avatar', () => {
     cy.get('.avatar-letter-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match image avatar', () => {
     cy.get('.avatar-image-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match list avatar', () => {
     cy.get('.avatar-list-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match list dense avatar', () => {
     cy.get('.avatar-list-dense-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   ////////
   // BADGES
@@ -48,7 +48,7 @@ context('UI Elements', () => {
     cy.get('.badge-side-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   ////////
   // BUTTONS
@@ -58,55 +58,55 @@ context('UI Elements', () => {
     cy.get('.button-default-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match button stroked', () => {
     cy.get('.button-stroked-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match button flat', () => {
     cy.get('.button-flat-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match button text with icon', () => {
     cy.get('.button-text-icon-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match button density', () => {
     cy.get('.button-density-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match button icon', () => {
     cy.get('.button-icon-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match button icon dense', () => {
     cy.get('.button-icon-dense-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match button FAB', () => {
     cy.get('.button-fab-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match button FAB mini', () => {
     cy.get('.button-fab-mini-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   ////////
   // TOGGLE
@@ -116,19 +116,19 @@ context('UI Elements', () => {
     cy.get('.toggle-filter-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
-  
+  });
+
   it('match toggle dense', () => {
     cy.get('.toggle-dense-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
-  
+  });
+
   it('match toggle group', () => {
     cy.get('.toggle-group-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   ////////
   // CARDS
@@ -138,25 +138,25 @@ context('UI Elements', () => {
     cy.get('.card-basic-test', { timeout: 5000 })
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match card workspace dense', () => {
     cy.get('.card-workspace-dense-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match card product dense', () => {
     cy.get('.card-product-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match card solution dense', () => {
     cy.get('.card-solution-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   ////////
   // CHIPS
@@ -166,48 +166,47 @@ context('UI Elements', () => {
     cy.get('.chip-states-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match chip densities', () => {
     cy.get('.chip-density-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match chip dense', () => {
     cy.get('.chip-dense-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match chip icon', () => {
     cy.get('.chip-icon-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match chip icon dense', () => {
     cy.get('.chip-icon-dense-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match chip removable', () => {
     cy.get('.chip-removable-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match chip avatar', () => {
     cy.get('.chip-avatar-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
+  });
 
   it('match chip toggle', () => {
     cy.get('.chip-toggle-test')
       .scrollIntoView()
       .toMatchImageSnapshot();
-  })
-
-})
+  });
+});
