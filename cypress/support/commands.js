e020a7d4061d5b59fd8f45ce9b980490640c0593
Thomas TALBOT <thomas.talbot@gmail.com>
2020-04-27 17:25:32 +0200

    fix(config): changing to dif from percent to pixels

--- cypress/support/commands.js
+++
@@ -1,6 +0,0 @@
-import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
-
-addMatchImageSnapshotCommand({
-  failureThreshold: 0.03, // threshold for entire image
-  failureThresholdType: 'pixels', // percent of image or number of pixels
-});
