f18f3eb6564431f719997c3e586e160acfb2794b
David <david.bocle@finastra.com>
2020-05-15 09:49:04 +0200

    test(global-nav): fix unit tests

--- libs/angular-components/global-nav/src/global-nav.component.spec.ts
+++ libs/angular-components/global-nav/src/global-nav.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GlobalNavComponent } from './global-nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -25,7 +26,8 @@ describe('GlobalNavModule', () => {
         MatButtonModule,
         MatCardModule,
         MatListModule,
-        NoopAnimationsModule
+        NoopAnimationsModule,
+        RouterTestingModule
       ],
       declarations: [GlobalNavComponent, NavbarComponent, SidenavComponent]
     }).compileComponents();
