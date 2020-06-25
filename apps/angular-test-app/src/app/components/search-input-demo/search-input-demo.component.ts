626040d0ce5bb9e1b025949895a5de5dc1b18ac4
Nguyen Thai Vinh <code2nguyen@gmail.com>
2020-06-25 14:33:02 +0200

    feat: implement search-input component

---
+++ apps/angular-test-app/src/app/components/search-input-demo/search-input-demo.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'ffdc-search-input-demo',
+  templateUrl: './search-input-demo.component.html',
+  styleUrls: ['./search-input-demo.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SearchInputDemoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
