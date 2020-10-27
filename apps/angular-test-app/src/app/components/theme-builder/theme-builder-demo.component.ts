352035be096cd139bbe0a8f7caa44a2c1d3a482b
Talbot, Thomas <thomas.talbot@finastra.com>
2020-10-27 22:08:39 +0100

    fix(theme): fix linting

--- apps/angular-test-app/src/app/components/theme-builder/theme-builder-demo.component.ts
+++ apps/angular-test-app/src/app/components/theme-builder/theme-builder-demo.component.ts
@@ -7,24 +7,42 @@ import { ColorEvent } from 'ngx-color';
   styleUrls: ['./theme-builder-demo.component.scss']
 })
 export class ThemeBuilderComponent {
+  colorShift = 150;
   statePrimary = '0, 158, 224';
   stateSecondary = '16, 71, 224';
   constructor() {}
 
+  private rgbStringFrom(rgbObject: any) {
+    return rgbObject.r + ',' + rgbObject.g + ',' + rgbObject.b;
+  }
+
+  private changeLuminosity(rgbObject: any, shift: number) {
+    const newObject = Object.assign({}, rgbObject);
+    Object.keys(newObject).map(function (channel, color) {
+      newObject[channel] += shift;
+    });
+    return newObject;
+  }
+
   changePrimaryComplete($event: ColorEvent) {
-    console.log($event);
+    document.documentElement.style.setProperty('--color-primary', this.rgbStringFrom($event.color.rgb));
     document.documentElement.style.setProperty(
-      '--color-primary',
-      $event.color.rgb.r + ',' + $event.color.rgb.g + ',' + $event.color.rgb.b
+      '--color-primary-lighter',
+      this.rgbStringFrom(this.changeLuminosity($event.color.rgb, this.colorShift))
     );
+    document.documentElement.style.setProperty(
+      '--color-primary-darker',
+      this.rgbStringFrom(this.changeLuminosity($event.color.rgb, -this.colorShift))
+    );
+    document.documentElement.style.setProperty('--gradient-stop-1', $event.color.hex);
+    document.documentElement.style.setProperty('--gradient-stop-2', $event.color.hex);
     //document.documentElement.style.setProperty("--color-primary-100", $event.color.rgb.r+', '+$event.color.rgb.g+', '+$event.color.rgb.b);
   }
 
   changeSecondaryComplete($event: ColorEvent) {
-    console.log($event);
-    document.documentElement.style.setProperty(
-      '--color-secondary',
-      $event.color.rgb.r + ',' + $event.color.rgb.g + ',' + $event.color.rgb.b
-    );
+    document.documentElement.style.setProperty('--color-secondary', this.rgbStringFrom($event.color.rgb));
+    document.documentElement.style.setProperty('--gradient-stop-3', $event.color.hex);
+    document.documentElement.style.setProperty('--gradient-stop-3-vert', $event.color.hex);
+    document.documentElement.style.setProperty('--gradient-stop-4', $event.color.hex);
   }
 }
