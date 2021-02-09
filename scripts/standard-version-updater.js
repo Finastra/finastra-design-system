72b796f41be6aed9cbd0ce2fbdb7ae989e54339c
Octavian-Ștefan Stoica <33375759+Octavians98@users.noreply.github.com>
2021-02-09 16:07:51 +0200

    fix(theme): make dark theme AA compliant (#256)

--- scripts/standard-version-updater.js
+++ scripts/standard-version-updater.js
@@ -1,17 +1,17 @@
 // standard-version-updater.js
-const stringifyPackage = require('stringify-package')
-const detectIndent = require('detect-indent')
-const detectNewline = require('detect-newline')
+const stringifyPackage = require('stringify-package');
+const detectIndent = require('detect-indent');
+const detectNewline = require('detect-newline');
 
 module.exports.readVersion = function (contents) {
-  return JSON.parse(contents).version
-}
+  return JSON.parse(contents).version;
+};
 
 module.exports.writeVersion = function (contents, version) {
-  const json = JSON.parse(contents)
-  let indent = detectIndent(contents).indent
-  let newline = detectNewline(contents)
-  json.version = version
-  json.dependencies["@ffdc/uxg-angular-theme"] = version
-  return stringifyPackage(json, indent, newline)
-}
+  const json = JSON.parse(contents);
+  let indent = detectIndent(contents).indent;
+  let newline = detectNewline(contents);
+  json.version = version;
+  json.dependencies['@ffdc/uxg-angular-theme'] = version;
+  return stringifyPackage(json, indent, newline);
+};
