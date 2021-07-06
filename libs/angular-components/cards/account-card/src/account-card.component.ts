b8b19a51841df2fdfe2c316c63f870ca08d79a0d
frigui dorsaf <dorsafrigui@gmail.com>
2021-07-06 14:50:05 +0200

    add onClick event and hover effect

--- libs/angular-components/cards/account-card/src/account-card.component.ts
+++ libs/angular-components/cards/account-card/src/account-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'uxg-account-card',
@@ -6,17 +6,26 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
   styleUrls: ['./account-card.component.scss']
 })
 export class AccountCardComponent implements OnInit, OnDestroy {
+  selected: boolean = false;
   @Input() name?: string;
   @Input() balance?: number;
   @Input() currency?: string;
   @Input() number?: string;
   @Input() icon = 'credit_card';
   @Input() iconAriaLabel = 'Credit Card';
-  @Input() selected?: boolean;
-  @Input() selectedWithCheckIcon?: boolean;
+  @Input() selectable?: boolean;
+  @Input() selectedithCheckIcon?: boolean;
+  @Output() onClick = new EventEmitter<boolean>();
 
   ngOnInit() {}
   ngOnDestroy() {}
+
+  public onCardClick(selected: boolean) {
+    if (this.selectable) {
+      this.selected = !selected;
+      this.onClick.emit(this.selected);
+    }
+  }
 }
 
 // Skeleton
