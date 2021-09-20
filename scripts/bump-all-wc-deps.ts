f414671a376d561c11c8146a1152469d25302af6
Nguyen Thai Vinh <code2nguyen@gmail.com>
2021-09-20 11:28:29 +0200

    bump wc deps version

---
+++ scripts/bump-all-wc-deps.ts
@@ -0,0 +1,114 @@
+/**
+ * Update all dependencies for web components (Material, Lit)
+ */
+
+import { execFileSync } from 'child_process';
+import * as fs from 'fs';
+import * as glob from 'glob';
+import * as path from 'path';
+
+const packagesDir = path.resolve(__dirname, '..', '..', 'libs', 'web-components');
+
+interface PackageJson {
+  name: string;
+  dependencies?: { [mdcwPkg: string]: string };
+  devDependencies?: { [mdcwPkg: string]: string };
+}
+
+interface PackageVersions {
+  mdc: string;
+  mwc: string;
+  litElement: string;
+  litHtml: string;
+}
+
+function mapPackageVersion(packageName: string, versions: PackageVersions): string {
+  if (packageName.startsWith('@material/mwc-')) {
+    return versions.mwc;
+  }
+
+  if (packageName.startsWith('@material/')) {
+    return versions.mdc;
+  }
+
+  if (packageName === 'lit-element') {
+    return versions.litElement;
+  }
+
+  if (packageName === 'lit-html') {
+    return versions.litHtml;
+  }
+
+  return '';
+}
+
+function main() {
+  const latestVersions: PackageVersions = {
+    mdc: getPkgVersion('@material/base@latest'),
+    mwc: getPkgVersion('@material/mwc-base@latest'),
+    litHtml: getPkgVersion('lit-html@latest'),
+    litElement: getPkgVersion('lit-element@latest')
+  };
+  console.log(`Found latest MDC Web version: ${latestVersions.mdc}\n`);
+  console.log(`Found latest MWC Web version: ${latestVersions.mwc}\n`);
+  console.log(`Found latest Lit Element version: ${latestVersions.litElement}\n`);
+  console.log(`Found latest Lit Html version: ${latestVersions.litHtml}\n`);
+
+  const packageJsonPaths = glob.sync(path.join('*', 'package.json'), { cwd: packagesDir });
+  let anyChanged = false;
+  for (const relPath of packageJsonPaths) {
+    const absPath = path.join(packagesDir, relPath);
+    const pj = JSON.parse(fs.readFileSync(absPath, 'utf8')) as PackageJson;
+    if (!pj.dependencies && !pj.devDependencies) {
+      continue;
+    }
+    console.log(`Checking ${pj.name}`);
+    let changed = false;
+
+    const updateDependencies = (dependencies: Record<string, string>) => {
+      for (const [pkg, oldVersion] of Object.entries(dependencies)) {
+        const newVersion = mapPackageVersion(pkg, latestVersions);
+        if (newVersion) {
+          if (oldVersion !== newVersion.substring(1)) {
+            dependencies[pkg] = newVersion.substring(1);
+            console.log(`\tUpdating ${pkg} from ${oldVersion} to ${newVersion}`);
+            changed = true;
+            anyChanged = true;
+          }
+        }
+      }
+    };
+
+    if (pj.dependencies) {
+      updateDependencies(pj.dependencies);
+    }
+    if (pj.devDependencies) {
+      updateDependencies(pj.devDependencies);
+    }
+    if (changed) {
+      console.log(`\tWriting new package.json`);
+      fs.writeFileSync(absPath, JSON.stringify(pj, null, 2) + '\n', 'utf8');
+    }
+  }
+  if (anyChanged) {
+    // Set an output value for consumption by a GitHub Action.
+    // https://help.github.com/en/articles/development-tools-for-github-actions#set-an-output-parameter-set-output
+    console.log(`::set-output name=new-mdc-version::${latestVersions.mdc.substring(1)}`);
+    console.log(`::set-output name=new-mwc-version::${latestVersions.mwc.substring(1)}`);
+    console.log(`::set-output name=new-lit-html-version::${latestVersions.litHtml.substring(1)}`);
+    console.log(`::set-output name=new-lit-element-version::${latestVersions.litElement.substring(1)}`);
+    console.log(`\nRemember to run npm install!`);
+  }
+}
+
+function getPkgVersion(packageName: string): string {
+  const packageVersion =
+    '=' +
+    execFileSync('npm', ['info', packageName, 'version'], {
+      encoding: 'utf8'
+    }).trim();
+
+  return packageVersion;
+}
+
+main();
