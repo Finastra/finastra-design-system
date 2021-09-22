1518d1689f04031da1811689b10f9cfb845ffaf2
Nguyen Thai Vinh <code2nguyen@gmail.com>
2021-09-22 17:19:27 +0200

    fix format

--- scripts/update-release-wc-file.js
+++ scripts/update-release-wc-file.js
@@ -1,23 +1,21 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 function updateFile(version, byUser) {
   const now = new Date();
 
   fs.appendFileSync(
-    path.join(__dirname, "..", ".release_wc.txt"),
+    path.join(__dirname, '..', '.release_wc.txt'),
     `- [${now.toLocaleDateString(undefined, {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
     })}] Request release v${version} by ${byUser} \r\n`
   );
 }
 
 if (require.main === module) {
-  const version = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "..", "lerna.json"))
-  ).version;
+  const version = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'lerna.json'))).version;
   var actor = process.argv.slice(2);
   updateFile(version, actor);
   console.log(`::set-output name=release-version::${version}`);
