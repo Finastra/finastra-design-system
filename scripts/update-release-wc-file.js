d15419edf220f3c86d8fdeab9f91aff6cba9d579
Nguyen Thai Vinh <code2nguyen@gmail.com>
2021-09-27 10:16:46 +0200

    chore(config): wc deps + github action (#410)

---
+++ scripts/update-release-wc-file.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+function updateFile(version, byUser) {
+  const now = new Date();
+
+  fs.appendFileSync(
+    path.join(__dirname, '..', '.release_wc.txt'),
+    `- [${now.toLocaleDateString(undefined, {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    })}] Request release v${version} by ${byUser} \r\n`
+  );
+}
+
+if (require.main === module) {
+  const version = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'lerna.json'))).version;
+  var actor = process.argv.slice(2);
+  updateFile(version, actor);
+  console.log(`::set-output name=release-version::${version}`);
+}
