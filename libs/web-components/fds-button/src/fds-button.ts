ebbd7bfdd17f0b0542689ac7380658b2b961d326
frigui dorsaf <dorsafrigui@gmail.com>
2021-10-18 18:01:55 +0200

    fds button implem

---
+++ libs/web-components/fds-button/src/fds-button.ts
@@ -0,0 +1,55 @@
+import { customElement, LitElement, html, property } from 'lit-element';
+import { styles } from './styles.css';
+import '@material/mwc-button';
+
+@customElement('fds-button')
+export class FdsButton extends LitElement {
+  static styles = styles;
+
+  @property({ type: String })
+  label = '';
+
+  @property({ type: String })
+  icon = '';
+
+  @property({ type: Boolean })
+  unelevated = true;
+
+  @property({ type: Boolean })
+  outlined = false;
+
+  @property({ type: Boolean })
+  text = false;
+
+  @property({ type: Boolean })
+  dense = false;
+
+  @property({ type: Boolean })
+  disabled = false;
+
+  render() {
+    if (this.outlined || this.text) {
+      return html`<mwc-button
+        ?disabled="${this.disabled}"
+        ?dense="${this.dense}"
+        ?outlined="${this.outlined}"
+        label="${this.label}"
+        icon="${this.icon}"
+      ></mwc-button>`;
+    }
+
+    return html`<mwc-button
+      ?unelevated="${this.unelevated}"
+      ?disabled="${this.disabled}"
+      ?dense="${this.dense}"
+      label="${this.label}"
+      icon="${this.icon}"
+    ></mwc-button>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'fds-button': FdsButton;
+  }
+}
