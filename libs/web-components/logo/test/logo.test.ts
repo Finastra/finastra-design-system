5703b3f5f95bc3a22fde391cefaf8bc43ff3274a
David Boclé <bcldvd@users.noreply.github.com>
2021-12-14 08:02:42 +0100

    Unit tests for logo

---
+++ libs/web-components/logo/test/logo.test.ts
@@ -0,0 +1,13 @@
+
+import {html, fixture, expect, elementUpdated} from '@open-wc/testing';
+import {Logo} from '../src/logo.js';
+import '../src/logo.js';
+
+describe('Logo', () => {
+    it('loads accessibly', async () => {
+        const el: Logo = await fixture(html`<fds-logo></fds-logo>`);
+
+        await elementUpdated(el);
+        await expect(el).to.be.accessible();
+    });
+});
--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { playwrightLauncher } from '@web/test-runner-playwright';
 
 const packagesPath = 'libs/web-components';
-const excludedPackages = ['helpers'];
+const excludedPackages = ['_helpers'];
 const packages = fs
   .readdirSync(packagesPath)
   .filter(dir => excludedPackages.includes(dir) === false)
