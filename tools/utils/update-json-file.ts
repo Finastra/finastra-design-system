ab03d5cc28c66f49a00815ff25299375e193e3a0
Francine Ong <francine.o@live.com>
2021-12-27 14:09:33 +0100

    fix(wc generator): migration to angular 13 deprecated JSONparser, use jsonc-parser as recommended instead

--- tools/utils/update-json-file.ts
+++ tools/utils/update-json-file.ts
@@ -1,5 +1,5 @@
-import { parseJson, JsonParseMode } from '@angular-devkit/core';
 import { Tree } from '@angular-devkit/schematics/src/tree/interface';
+import { parse } from 'jsonc-parser';
 
 type UpdateJsonFn<T> = (obj: T) => T | void;
 
@@ -7,8 +7,8 @@ export function updateJsonFile<T>(host: Tree, path: string, callback: UpdateJson
   const source = host.read(path);
   if (source) {
     const sourceText = source.toString('utf-8');
-    const json = parseJson(sourceText, JsonParseMode.Loose);
-    callback((json as {}) as T);
+    const json = parse(sourceText);
+    callback(json as {} as T);
     host.overwrite(path, JSON.stringify(json, null, 2));
   }
   return host;
