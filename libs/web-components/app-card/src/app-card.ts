606704f85b6f72616c45960b951b8fab32348525
Francine Ong <francine.o@live.com>
2022-01-12 13:02:27 +0100

    wc(app card): remove primary attribute

--- libs/web-components/app-card/src/app-card.ts
+++ libs/web-components/app-card/src/app-card.ts
@@ -6,24 +6,18 @@ import { BaseCard } from '@finastra/base-card';
 import { styles } from './styles.css';
 import { renderPatternSVG, renderPrimarySVG, renderSecondarySVG } from './app-card-decorations';
 
-export type ColorType = 'primary' | 'secondary';
-
 @customElement('fds-app-card')
 export class AppCard extends BaseCard {
   static styles = styles;
 
   @property({ type: String }) label;
   @property({ type: String }) shortLabel;
-  @property({ type: Boolean }) primary = false;
   @property({ type: Boolean }) secondary = false;
   @property({ type: Boolean }) large = false;
   @property({ type: Boolean }) dense = false;
   @property({ type: Boolean }) extraDense = false;
 
   protected renderCardContent(): TemplateResult {
-    if (this.primary && this.secondary) {
-      throw new Error('Cannot use both primary and secondary attribute, default color is primary');
-    }
     if ([this.large, this.dense, this.extraDense].filter((val) => val === true).length > 1) {
       throw new Error('Cannot use multiple size attributes at the same time');
     }
