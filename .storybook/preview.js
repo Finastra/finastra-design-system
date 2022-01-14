063b13c050ddd22bd36a8835f945a0ea1f418ac7
FrancineO <francine.o@live.com>
2022-01-14 09:37:04 +0100

    Wc/app card (#543)
    * wc(base-card): separate base card from card and card extends base-card
    
    * wc(base-card): Card style uses base card style
    
    * wc(app card): create app card component extending base card
    
    * wc(app card): demo + style
    
    * wc(app card): style
    
    * wc(app card): storybook
    
    * wc(app card): do not allow multiple color or size properties
    
    * wc(base card): storybook
    
    * wc(app-card): test 100% coverage
    
    * wc(app card): add publish config
    
    * wc(app card): change versions and add base card dependency
    
    * wc(app card): upgrade versions
    
    * Add base-card to tsconfig
    
    * wc(app-card): fix extraDense using camelCase
    
    * wc(app-card): upgrade versions
    
    * package-lock updated
    
    * wc(app-card): add base-card dependency
    
    * wc(app-card): try fix build by referencing base-card before
    
    * wc(app-card): format
    
    * feat(storybook): Sort stories alphabetically
    
    * formatting
    
    * wc(app card): change display name
    
    * wc(app-card): move decoration functions in external file
    
    * wc(app card): remove primary attribute
    
    * wc(app card): log warning if multiple size attributes
    
    * wc(app card): add examples in storybook
    
    * wc(app card): storybook order consistency
    
    * wc(app card): README
    
    * wc(app card): remove unused css
    
    * wc(app-card): apply default size if multiple size attributes
    
    * wc: update global readme
    
    * wc(app-card): add style to make text accessible in dark mode
    
    * wc(base card): add publishconfig
    
    Co-authored-by: David Boclé <bcldvd@users.noreply.github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,10 +4,11 @@ export const parameters = {
   layout: 'centered',
   options: {
     storySort: {
-      order: ['Foundations', 'Components']
+      order: ['Foundations', 'Components'],
+      method: 'alphabetical'
     }
   },
   docs: {
-    theme: theme,
-  },
+    theme: theme
+  }
 };
