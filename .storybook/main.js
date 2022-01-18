28e61b0bda7deb7dd5298b951a7c5f2a50d2e209
David Boclé <bcldvd@users.noreply.github.com>
2022-01-18 17:48:10 +0100

    fix foundations

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../libs/web-components/**/stories/*.stories.{js,md,mdx}', '../themes/fds-theme/**/*.mdx'],
+  stories: ['../libs/web-components/**/stories/*.stories.{js,md,mdx}', '../themes/fds-theme/**/*.stories.mdx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-dark-mode', '@storybook/preset-scss'],
   framework: '@storybook/web-components',
   core: {
