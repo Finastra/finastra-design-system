9ab7b139f0ba846d1d1a56fdc4477314176956de
David Boclé <bcldvd@users.noreply.github.com>
2022-01-18 11:28:20 +0100

    Add dark theme

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../libs/web-components/**/stories/*.stories.{js,md,mdx}', '../themes/fds-theme/**/*.mdx'],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
-  "framework": "@storybook/web-components",
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-dark-mode'],
+  framework: '@storybook/web-components',
   core: {
-    builder: "webpack5"
+    builder: 'webpack5'
   }
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
+import { themes } from '@storybook/theming';
 import theme from './theme';
 import '../themes/fds-theme/prebuilt/all-themes.css';
+import './all-themes.scss';
+import addons from '@storybook/addons';
+
+const isDark =
+    typeof window !== `undefined`
+        ? window.matchMedia &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+        : null
 
 export const parameters = {
   layout: 'centered',
@@ -11,5 +20,27 @@ export const parameters = {
   },
   docs: {
     theme: theme
+  },
+  darkMode: {
+    current: isDark ? "dark" : "light",
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: '#191919' },
+    // Override the default light theme
+    light: { ...themes.normal },
+    darkClass: 'dark-theme',
+    lightClass: 'light-theme',
+    stylePreview: true,
   }
 };
+
+
+// get an instance to the communication channel for the manager and preview
+const channel = addons.getChannel();
+
+channel.on('DARK_MODE', (isDark) => {
+  if (isDark) {
+    console.log('dark')
+  } else {
+    console.log('light')
+  }
+});
