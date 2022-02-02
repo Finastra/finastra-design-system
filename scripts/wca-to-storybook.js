225ef332b019ed637842a1acbb740fcc93e64ad3
frigui dorsaf <dorsafrigui@gmail.com>
2022-02-02 11:21:34 +0100

    format

--- scripts/wca-to-storybook.js
+++ scripts/wca-to-storybook.js
@@ -1,29 +1,29 @@
 const { readFileSync, writeFileSync } = require('fs');
 const { join } = require('path');
 const globby = require('globby');
-const READ_WRITE_OPTS = {encoding: 'utf-8'};
+const READ_WRITE_OPTS = { encoding: 'utf-8' };
 
 async function main() {
-    const paths = await getPaths('../libs/web-components/*/stories/custom-element.json');
-    paths.forEach(path => {
+  const paths = await getPaths('../libs/web-components/*/stories/custom-element.json');
+  paths.forEach((path) => {
     const { attributes, slots, cssProperties } = getContent(path);
     const argTypes = mapArgTypes(attributes, slots);
-        const cssprops = mapCssProps(cssProperties);
+    const cssprops = mapCssProps(cssProperties);
     const newFile = JSON.stringify({ argTypes, cssprops }, null, 2);
-        writeFileSync(path, newFile, READ_WRITE_OPTS);
-    });
+    writeFileSync(path, newFile, READ_WRITE_OPTS);
+  });
 }
 
 main();
 
 async function getPaths(paths) {
-    const eatThat = join(__dirname, paths);
-    return await globby(eatThat);
+  const eatThat = join(__dirname, paths);
+  return await globby(eatThat);
 }
 
 function getContent(path) {
-    const wcaOutput = readFileSync(path, READ_WRITE_OPTS);
-    return JSON.parse(wcaOutput).tags[0];
+  const wcaOutput = readFileSync(path, READ_WRITE_OPTS);
+  return JSON.parse(wcaOutput).tags[0];
 }
 
 function mapArgTypes(attributes, slots) {
@@ -39,9 +39,9 @@ function mapArgTypes(attributes, slots) {
   if (attributes) {
     attr = attributes.reduce((prev, next) => {
       prev[next.name] = { control: sanitizeControl(next.type) };
-        return prev;
+      return prev;
     }, {});
-}
+  }
 
   return {
     ...attr,
@@ -50,26 +50,26 @@ function mapArgTypes(attributes, slots) {
 }
 
 function mapCssProps(cssProperties) {
-    if (!cssProperties) return {};
-    return cssProperties.reduce((prev, next) => {
-        prev[sanitizeCssPropName(next.name)] = {
-            ...(next.description ? {description: next.description} : {}),
-            ...(next.default ? {value: sanitizeValue(next.default)} : {}),
-            control: next.type || 'text'
-        };
-        return prev;
-    }, {});
+  if (!cssProperties) return {};
+  return cssProperties.reduce((prev, next) => {
+    prev[sanitizeCssPropName(next.name)] = {
+      ...(next.description ? { description: next.description } : {}),
+      ...(next.default ? { value: sanitizeValue(next.default) } : {}),
+      control: next.type || 'text'
+    };
+    return prev;
+  }, {});
 }
 
 function sanitizeControl(control) {
-    if (control === 'string') control = 'text';
-    return control || 'text';
+  if (control === 'string') control = 'text';
+  return control || 'text';
 }
 
 function sanitizeValue(value) {
-    return value.replace(/^\"(.*)\"$/g, "$1");
+  return value.replace(/^\"(.*)\"$/g, '$1');
 }
 
 function sanitizeCssPropName(name) {
-    return name.replace(/^--(.*)/g, "$1");
+  return name.replace(/^--(.*)/g, '$1');
 }
