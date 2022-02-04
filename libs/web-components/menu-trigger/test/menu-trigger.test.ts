6d99c8e0fd0e17448f93b5f327ec99cd74add1dd
pi5rr5 <pierre.simon@finastra.com>
2022-02-04 11:15:16 +0100

    feat(launchpad): test menu-trigger

--- libs/web-components/menu-trigger/test/menu-trigger.test.ts
+++ libs/web-components/menu-trigger/test/menu-trigger.test.ts
@@ -9,4 +9,22 @@ describe('MenuTrigger', () => {
     await elementUpdated(el);
     await expect(el).to.be.accessible();
   });
+
+  it('should be outline by default', async () => {
+    const el: MenuTrigger = await fixture(html`<fds-menu-trigger></fds-menu-trigger>`);
+
+    await elementUpdated(el);
+    await expect(shadowRoot(el).querySelector('mwc-button')?.hasAttribute('outlined')).to.equal(true);
+  });
+
+  it('should toggle', async () => {
+    const el: MenuTrigger = await fixture(html`<fds-menu-trigger></fds-menu-trigger>`);
+    await elementUpdated(el);
+    await shadowRoot(el).querySelector('fds-menu-trigger')?.click();
+    await expect(el.on && el.icon === "expand_less").to.be.true;
+  });
 });
+
+function shadowRoot(el: Element) {
+  return el.shadowRoot ? el.shadowRoot : el;
+}
