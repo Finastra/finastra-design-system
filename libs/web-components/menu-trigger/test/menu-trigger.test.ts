a8eab3e58a99f647c59592be186aa338a596a92d
pi5rr5 <sim.pierre91@gmail.com>
2022-02-08 12:50:08 +0100

    feat(launchpad): fix menu-trigger test

--- libs/web-components/menu-trigger/test/menu-trigger.test.ts
+++ libs/web-components/menu-trigger/test/menu-trigger.test.ts
@@ -4,23 +4,23 @@ import '../src/menu-trigger.js';
 
 describe('MenuTrigger', () => {
   it('loads accessibly', async () => {
-    const el: MenuTrigger = await fixture(html`<fds-menu-trigger></fds-menu-trigger>`);
+    const el: MenuTrigger = await fixture(html`<fds-menu-trigger outlined></fds-menu-trigger>`);
 
     await elementUpdated(el);
     await expect(el).to.be.accessible();
   });
 
-  it('should be outline by default', async () => {
-    const el: MenuTrigger = await fixture(html`<fds-menu-trigger></fds-menu-trigger>`);
+  it('should be outline', async () => {
+    const el: MenuTrigger = await fixture(html`<fds-menu-trigger outlined></fds-menu-trigger>`);
     await elementUpdated(el);
-    await expect(shadowRoot(el).querySelector('mwc-button')?.hasAttribute('outlined')).to.equal(true);
+    await expect(shadowRoot(el).querySelector('fds-button')?.hasAttribute('outlined')).to.equal(true);
   });
 
   it('should toggle', async () => {
-    const el: MenuTrigger = await fixture(html`<fds-menu-trigger></fds-menu-trigger>`);
+    const el: MenuTrigger = await fixture(html`<fds-menu-trigger outlined></fds-menu-trigger>`);
     await elementUpdated(el);
-    await shadowRoot(el).querySelector('mwc-button')?.click();
-    await expect(el.on && el.icon === "expand_less").to.be.true;
+    await shadowRoot(el).querySelector('fds-button')?.click();
+    await expect(el.on && el.menuTrailingIcon === "arrow_drop_up").to.be.true;
   });
 });
 
