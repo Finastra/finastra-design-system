d286f7d14042e6061c40f3c745b408f45178bd05
Nguyen Thai Vinh <code2nguyen@gmail.com>
2022-02-23 13:43:47 +0100

    Feature/auto complete part 1 (#569)
    * implement auto-complete part 1
    
    * merge master
    
    * add keyboard navigation
    
    * update package-lock
    
    * update version
    
    * autocomplete: add loading property
    
    * add autocomplete unit tests
    
    * Update autocomplete.stories.mdx
    
    * fix: wc-build for autocomplete
    
    * remove @soyPrefixAttribute
    
    * fix useInnerFilter in storybook
    
    * fix(autocomplete): menu text color
    
    * update search input version
    
    * Revert "update search input version"
    
    This reverts commit 64e8d6c3b698f391bec9700299d3fab5c7e08f0a.
    
    * revert package lock
    
    * storybook in ts
    
    * update package-lock
    
    * fix label color
    
    * support placeholder
    
    * feat: Add placeholder to autocomplete
    
    Co-authored-by: David Boclé <bcldvd@users.noreply.github.com>
    Co-authored-by: frigui dorsaf <dorsafrigui@gmail.com>

--- libs/web-components/search-input/src/search-input.ts
+++ libs/web-components/search-input/src/search-input.ts
@@ -4,6 +4,7 @@ import { TextFieldBase } from '@material/mwc-textfield/mwc-textfield-base';
 import { classMap } from 'lit/directives/class-map.js';
 
 import '@material/mwc-icon-button';
+import '@material/mwc-circular-progress';
 
 import { styles } from './styles.css';
 
@@ -15,21 +16,35 @@ export class SearchInput extends TextFieldBase {
   static styles = styles;
 
   @property({ type: String }) icon = 'search';
+  @property({ type: Boolean }) showClearButton = true;
+  @property({ type: Boolean }) loading = false;
 
-  protected renderTrailingIcon(): TemplateResult | string {
+  protected renderClearButton(): TemplateResult | string {
     const clearButtonclasses = {
       'fds-search-input-clear-button--show': !!this.value,
       'fds-search-input-clear-button--hide': !this.value
     };
-    return html`<mwc-icon-button
-      @click=${this.clear}
-      class="mdc-text-field__affix--suffix fds-search-input-clear-button ${classMap(clearButtonclasses)}"
-      icon="clear"
-    ></mwc-icon-button>`;
+    return this.showClearButton
+      ? html`<mwc-icon-button
+          @click=${this.clear}
+          class="mdc-text-field__affix--suffix fds-search-input-clear-button ${classMap(clearButtonclasses)}"
+          icon="clear"
+        ></mwc-icon-button>`
+      : ``;
+  }
+
+  protected renderLoadingButton() : TemplateResult {
+    return html`<mwc-circular-progress indeterminate density="-6" class="mdc-text-field__affix--suffix" ></mwc-circular-progress>`
+  }
+
+  protected renderTrailingIcon(): TemplateResult | string {
+    return this.loading ? this.renderLoadingButton() : this.renderClearButton()
   }
 
   private clear() {
     this.value = '';
+    const inputEvent = new CustomEvent('input');
+    this.dispatchEvent(inputEvent);
     this.focus();
   }
 }
