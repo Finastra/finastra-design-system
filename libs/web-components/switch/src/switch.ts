28075ec1b35a4deac5e71437ce72bedf1faf83fb
frigui dorsaf <dorsafrigui@gmail.com>
2022-03-14 18:00:24 +0100

    use fds colors

--- libs/web-components/switch/src/switch.ts
+++ libs/web-components/switch/src/switch.ts
@@ -2,12 +2,21 @@ import { customElement } from 'lit/decorators.js';
 import { SwitchBase } from '@material/mwc-switch/mwc-switch-base';
 
 import { styles } from './styles.css';
+import { html, TemplateResult } from 'lit-html';
 
 @customElement('fds-switch')
 export class Switch extends SwitchBase {
-  static override styles = [styles];  
+  static override styles = [styles];
   constructor() {
     super();
+  }
+
+  renderHandle(): TemplateResult {
+    return html`
+      <div class="mdc-switch__handle">
+        <div class="mdc-switch__icons"></div>
+      </div>
+    `;
   }
 }
 
