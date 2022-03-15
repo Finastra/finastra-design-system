fa18d7e662697d17ac3089b1d1e2c67e7216a58b
pi5rr5 <sim.pierre91@gmail.com>
2022-03-15 13:00:35 +0100

    feature(#632): use comment to document radio api

--- libs/web-components/radio/src/radio.ts
+++ libs/web-components/radio/src/radio.ts
@@ -3,8 +3,15 @@ import { customElement } from 'lit/decorators.js';
 import { styles } from './styles.css';
 
 /**
- * @cssprop {color} [--fds-primary=#917EE0] - FDS Primary color
+ * @cssprop {color} [--fds-primary=#917EE0] - FDS Primary color.
+ * @attr [checked=true] - Whether this radio button is the currently-selected one in its group.
+ * @attr [disabled=false] - If true, this radio button cannot be selected or de-selected.
+ * @attr [name=''] - Name of the input for form submission, and identifier for the selection group. Only one radio button can be checked for a given selection group.
+ * @attr [value=''] - Value of the input for form submission.
+ * @attr [global=false] - If true, this radio button will use a global, document-level scope for its selection group rather than its local shadow root.
+ * @attr [reducedTouchTarget=false] - When true, the radio removes touch target that extends beyond visual boundary of the component.
  */
+
 @customElement('fds-radio')
 export class Radio extends RadioBase {
   static override styles = [styles];
--- libs/web-components/radio/stories/radio.stories.ts
+++ libs/web-components/radio/stories/radio.stories.ts
@@ -3,73 +3,12 @@ import '@finastra/radio';
 import type { Radio } from '@finastra/radio';
 import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
-import { cssprops } from './custom-element.json';
+import { argTypes, cssprops } from './custom-element.json';
 
 export default {
   title: 'Components/Radio',
   component: 'fds-radio',
-  argTypes: {
-    checked: {
-      table: {
-        category: 'attributes',
-        defaultValue: { summary: false }
-      },
-      control: 'boolean',
-      defaultValue: false,
-      type: 'boolean',
-      description: 'Whether this radio button is the currently-selected one in its group.'
-    },
-    disabled: {
-      table: {
-        category: 'attributes',
-        defaultValue: { summary: false }
-      },
-      control: 'boolean',
-      defaultValue: false,
-      type: 'boolean',
-      description: 'If true, this radio button cannot be selected or de-selected.'
-    },
-    name: {
-      table: {
-        category: 'attributes',
-        defaultValue: { summary: '' }
-      },
-      control: 'text',
-      defaultValue: '',
-      type: 'string',
-      description: 'Name of the input for form submission, and identifier for the selection group. Only one radio button can be checked for a given selection group.'
-    },
-    value: {
-      table: {
-        category: 'attributes',
-        defaultValue: { summary: '' }
-      },
-      control: 'text',
-      defaultValue: '',
-      type: 'string',
-      description: 'Value of the input for form submission.'
-    },
-    global: {
-      table: {
-        category: 'attributes',
-        defaultValue: { summary: false }
-      },
-      control: 'boolean',
-      defaultValue: false,
-      type: 'boolean',
-      description: 'If true, this radio button will use a global, document-level scope for its selection group rather than its local shadow root.'
-    },
-    reducedTouchTarget: {
-      table: {
-        category: 'attributes',
-        defaultValue: { summary: false }
-      },
-      control: 'boolean',
-      defaultValue: false,
-      type: 'boolean',
-      description: 'When true, the radio removes touch target that extends beyond visual boundary of the component.'
-    },
-  },
+  argTypes,
   parameters: {
     docs: {
       description: { component: README }
