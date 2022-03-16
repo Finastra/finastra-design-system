a8108ae82d5ff9036c96df72b0ec9060ae13c0e5
frigui dorsaf <dorsafrigui@gmail.com>
2022-03-16 11:10:06 +0100

    correct colors

--- libs/web-components/switch/src/switch.ts
+++ libs/web-components/switch/src/switch.ts
@@ -4,6 +4,13 @@ import { SwitchBase } from '@material/mwc-switch/mwc-switch-base';
 import { styles } from './styles.css';
 import { html, TemplateResult } from 'lit-html';
 
+/**
+ * @cssprop {color} [--fds-primary=#694ED6] - Switch color
+ * @cssprop {color} [--fds-track-opacity=0.54] - track color opacity
+ * @attr [selected=false] - Whether the checkbox is checked.
+ * @attr [disabled=false] - When true, the checkbox cannot be interacted with.
+ */
+
 @customElement('fds-switch')
 export class Switch extends SwitchBase {
   static override styles = [styles];
