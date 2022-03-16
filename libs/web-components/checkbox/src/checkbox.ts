f92231babe07de8e099c9c321ee9a896e77226da
dorsaf frigui <dorsafrigui@gmail.com>
2022-03-16 10:23:07 +0100

    correct css prop description

--- libs/web-components/checkbox/src/checkbox.ts
+++ libs/web-components/checkbox/src/checkbox.ts
@@ -5,7 +5,7 @@ import { styles } from './styles.css';
 import { CheckboxBase } from '@material/mwc-checkbox/mwc-checkbox-base';
 
 /**
- * @cssprop {color} [--fds-primary=#694ED6] - Color of the start of the gradient
+ * @cssprop {color} [--fds-primary=#694ED6] - Checkbox color
  * @attr [checked=true] - Whether the checkbox is checked.
  * @attr [indeterminate=false] - It is used on the parent to indicate that some but not all of its children are checked.
  * @attr [disabled=false] - When true, the checkbox cannot be interacted with.
