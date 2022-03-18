89184b66e2a01539a2a17e30300f9103f4b535e9
frigui dorsaf <dorsafrigui@gmail.com>
2022-03-18 16:31:06 +0100

    storybook examples

--- libs/web-components/textfield/src/textfield.ts
+++ libs/web-components/textfield/src/textfield.ts
@@ -1,17 +1,24 @@
-import { LitElement, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
+import {TextFieldBase} from '@material/mwc-textfield/mwc-textfield-base';
 
 import { styles } from './styles.css';
 
+/**
+ * @cssprop {color} [--fds-primary=#694ED6] - Textarea color.
+ * @attr [label='textfield'] - Sets floating label value.
+ * @attr [icon=''] - Leading icon to display in input. See mwc-icon.
+ * @attr [type=text] - A string specifying the type of control to render.
+ * @attr [validationMessage='error'] - Message to show in the error color when the textfield is invalid. (Helper text will not be visible)
+ * @attr [disabled=false] - Whether or not the input should be disabled.
+ * @attr [outlined=false] - Whether or not to show the material outlined variant.
+ */
+
 @customElement('fds-textfield')
-export class Textfield extends LitElement {
+export class Textfield extends TextFieldBase {
   static styles = styles;
 
-  @property({ type: String })
-  name = '';
-
-  render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+  constructor() {
+    super();
   }
 }
 
--- libs/web-components/textfield/stories/textfield.stories.ts
+++ libs/web-components/textfield/stories/textfield.stories.ts
@@ -3,27 +3,59 @@ import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
 import '@finastra/textfield';
 import type { Textfield } from '@finastra/textfield';
+import { argTypes, cssprops } from './custom-element.json';
 
 export default {
   title: 'Components/Textfield',
   component: 'fds-textfield',
-  args: {
-    name: 'World'
-  },
+  argTypes,
   parameters: {
     docs: {
       description: { component: README }
     }
   },
-  decorators: [
-    (story) => html`${story()}<style>
-        /* Add you styles here */
-      </style>`
-  ]
+  cssprops
 } as Meta;
 
-const Template: Story<Textfield> = ({ name = 'World' }) => {
-  return html`<fds-textfield  .name=${name}></fsd-textfield>`;
+const Template: Story<Textfield> = ({ label, icon, disabled, outlined }) => {
+  return html`<fds-textfield label=${label} icon=${icon} ?disabled=${disabled} ?outlined=${outlined}></fsd-textfield>`;
+};
+
+const ValidationTemplate: Story<Textfield> = ({ label, icon, type, validationMessage, disabled, outlined }) => {
+  return html`<fds-textfield label=${label} icon=${icon} type=${type} validationMessage=${validationMessage} ?disabled=${disabled} ?outlined=${outlined}></fsd-textfield>`;
 };
 
 export const Default: Story<Textfield> = Template.bind({});
+Default.args = {
+  label: 'Default',
+  icon: 'event'
+};
+
+export const Password: Story<Textfield> = ValidationTemplate.bind({});
+Password.args = {
+  label: 'Enter your password',
+  type: 'password',
+  icon: 'event'
+};
+
+export const ErrorMessage: Story = ValidationTemplate.bind({});
+ErrorMessage.args = {
+  type: 'email',
+  validationMessage: 'Not a valid email',
+  label: 'Enter your email',
+  icon: 'event'
+};
+
+export const Outlined: Story<Textfield> = Template.bind({});
+Outlined.args = {
+  label: 'Outlined textfield',
+  icon: 'event',
+  outlined: true
+};
+
+export const Disabled: Story<Textfield> = Template.bind({});
+Disabled.args = {
+  label: 'Disabled',
+  icon: 'event',
+  disabled: true
+};
