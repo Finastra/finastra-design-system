7f209df2f37e2d70095d4f40a8c1730bb48ec488
dorsaf frigui <dorsafrigui@gmail.com>
2022-03-23 09:52:49 +0100

    feature(wc): fds-textarea (#640)

---
+++ libs/web-components/textarea/README.md
@@ -0,0 +1,14 @@
+# Textarea
+Text areas let users enter and edit text.
+
+It extends Material web `mwc-textarea-base`. For usage please see [mwc-textarea](https://github.com/material-components/material-web/tree/master/packages/textarea).
+
+[![See it on NPM!](https://img.shields.io/npm/v/@finastra/textarea?style=for-the-badge)](https://www.npmjs.com/package/@finastra/textarea)
+[![How big is this package in your project?](https://img.shields.io/bundlephobia/minzip/@finastra/textarea?style=for-the-badge)](https://bundlephobia.com/result?p=@finastra/textarea')
+[![Storybook](https://shields.io/badge/-Play%20with%20this%20web%20component-2a0481?logo=storybook&style=for-the-badge)](https://finastra.github.io/finastra-design-system/?path=/story/components-textarea--default)
+
+## Installation
+
+```
+npm i @finastra/textarea
+```
---
+++ libs/web-components/textarea/stories/textarea.stories.ts
@@ -0,0 +1,52 @@
+const README = require('../README.md');
+import { Meta, Story } from '@storybook/web-components';
+import { html } from 'lit-html';
+import '@finastra/textarea';
+import type { Textarea } from '@finastra/textarea';
+import { argTypes, cssprops } from './custom-element.json';
+
+export default {
+  title: 'Components/Textarea',
+  component: 'fds-textarea',
+  argTypes,
+  args: {
+    label: 'Textarea',
+    helper: 'Helper text'
+  },
+  parameters: {
+    docs: {
+      description: { component: README }
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/E1Mb1556RT3HbAUVu2Q0LV/Finastra-design-system?node-id=16553%3A22669'
+    },
+    cssprops
+  },
+} as Meta;
+
+const Template: Story<Textarea> = ({ label, helper, required, disabled }) => {
+  return html`<fds-textarea ?required=${required} label=${label} helper=${helper} ?disabled=${disabled}></fsd-textarea>`;
+};
+
+const CounterTemplate: Story<Textarea> = ({ label, helper, maxLength, charCounter, required, disabled }) => {
+  return html`<fds-textarea charCounter=${charCounter} maxLength=${maxLength} ?required=${required} label=${label} helper=${helper} ?disabled=${disabled}></fsd-textarea>`;
+};
+
+export const Default: Story<Textarea> = Template.bind({});
+
+export const Required: Story<Textarea> = Template.bind({});
+Required.args = {
+  required: true
+};
+
+export const CharacterCounter: Story<Textarea> = CounterTemplate.bind({});
+CharacterCounter.args = {
+  maxLength: 18,
+  charCounter: true,
+};
+
+export const Disabled: Story<Textarea> = Template.bind({});
+Disabled.args = {
+  disabled: true
+};
