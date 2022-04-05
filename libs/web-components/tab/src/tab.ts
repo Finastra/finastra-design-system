6c560fb9435fd72885f713ee03ef0e7310dd329d
frigui dorsaf <dorsafrigui@gmail.com>
2022-04-05 11:01:18 +0100

    implement tab story

--- libs/web-components/tab/src/tab.ts
+++ libs/web-components/tab/src/tab.ts
@@ -1,12 +1,24 @@
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { TabBase } from '@material/mwc-tab/mwc-tab-base';
 
 import { styles } from './styles.css';
+
+/**
+ * @attr [label=''] - Text label to display in tab.
+ * @attr [icon=''] - Material design icon name to display.
+ * @attr [stacked='false'] - Stacks icon on top of label text.
+ * @attr [classic='false'] - displays the fds tab.
+ * @attr [segmented='false'] - displays a segmented tab, very similar to a button.
+ */
 
 @customElement('fds-tab')
 export class Tab extends TabBase {
   static styles = styles;
 
+  @property({ type: Boolean }) classic = false;
+  @property({ type: Boolean }) segmented = false;
+  @property({ type: Boolean }) stacked = false;
+  
   constructor() {
     super();
   }
--- libs/web-components/tab/stories/tab.stories.ts
+++ libs/web-components/tab/stories/tab.stories.ts
@@ -2,28 +2,54 @@ const README = require('../README.md');
 import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
 import '@finastra/tab';
-import type { Tab } from '@finastra/tab';
+import '@material/mwc-tab-bar';
+import { argTypes, cssprops } from './custom-element.json';
 
 export default {
   title: 'Components/Tab',
   component: 'fds-tab',
-  args: {
-    name: 'World'
-  },
+  argTypes,
   parameters: {
     docs: {
       description: { component: README }
-    }
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/E1Mb1556RT3HbAUVu2Q0LV/Finastra-design-system?node-id=251%3A5493'
+    },
+    cssprops
   },
-  decorators: [
-    (story) => html`${story()}<style>
-        /* Add you styles here */
-      </style>`
-  ]
 } as Meta;
 
-const Template: Story<Tab> = ({ name = 'World' }) => {
-  return html`<fds-tab  .name=${name}></fsd-tab>`;
+const Template: Story = ({ label, icon, segmented, stacked, classic}) => {
+  return html`<mwc-tab-bar>
+  <fds-tab label=${label} icon=${icon} ?segmented=${segmented} ?classic=${classic} ?stacked=${stacked}></fds-tab>
+</mwc-tab-bar>`;
 };
 
-export const Default: Story<Tab> = Template.bind({});
+export const Default: Story = Template.bind({});
+Default.args = {
+  label: 'Active',
+  icon: "location_on",
+};
+
+export const Segmented: Story = Template.bind({});
+Segmented.args = {
+  label: 'active',
+  icon: "location_on",
+  segmented: true,
+};
+
+export const Classic: Story = Template.bind({});
+Classic.args = {
+  label: 'Active',
+  icon: "location_on",
+  classic: true
+};
+
+export const Stacked: Story = Template.bind({});
+Stacked.args = {
+  label: 'Active',
+  icon: "location_on",
+  stacked: true
+};
