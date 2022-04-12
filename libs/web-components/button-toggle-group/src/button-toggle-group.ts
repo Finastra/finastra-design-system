ac1084ca6a129e362b248c354ca4b501b94a5e22
frigui dorsaf <dorsafrigui@gmail.com>
2022-04-12 10:04:07 +0100

    add filter button toggle

--- libs/web-components/button-toggle-group/src/button-toggle-group.ts
+++ libs/web-components/button-toggle-group/src/button-toggle-group.ts
@@ -3,6 +3,11 @@ import { TabBarBase } from '@material/mwc-tab-bar/mwc-tab-bar-base';
 
 import { styles } from './styles.css';
 
+/**
+ * @cssprop {color} [ --fds-tab-min-height=90] - Textfield color
+ * @cssprop {color} [--fds-icon-color=#694ED6] - Icon color.
+ * @cssprop {color} [--fds-icon-trailing-color=#694ED6] - Icon trailing color.
+ * */
 @customElement('fds-button-toggle-group')
 export class ButtonToggleGroup extends TabBarBase {
   static styles = styles;
