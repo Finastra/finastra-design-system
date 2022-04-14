f63a9b5caee61b6f706e0c9a1bdf5377a3b99672
Talbot, Thomas <thomas.talbot@finastra.com>
2022-04-14 11:12:05 +0200

    feat(wc): add argtypes and cssprops

--- libs/web-components/slider/stories/slider.stories.ts
+++ libs/web-components/slider/stories/slider.stories.ts
@@ -3,10 +3,12 @@ import '@finastra/slider';
 import type { Slider } from '@finastra/slider';
 import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
+import { argTypes, cssprops } from './custom-element.json';
 
 export default {
   title: 'Components/Slider',
   component: 'fds-slider',
+  argTypes,
   parameters: {
     docs: {
       description: { component: README }
@@ -14,12 +16,13 @@ export default {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/E1Mb1556RT3HbAUVu2Q0LV/Finastra-design-system?node-id=48606%3A19616'
-    }
+    },
+    cssprops
   }
 } as Meta;
 
 const Template: Story<Slider> = ({value, min, max}) => {
-  return html`<fds-slider value=${value} min=${min} max=${max}></fds-slider>`;
+  return html`<fds-slider value=${value} ?min=${min} ?max=${max}></fds-slider>`;
 };
 
 export const Default: Story<Slider> = Template.bind({});
