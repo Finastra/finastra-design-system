429df0a2c7a765ff436d72e16fa3435f364542bf
Talbot, Thomas <thomas.talbot@finastra.com>
2022-04-15 13:24:58 +0200

    fix(slider): aria-label on input for accessibility

--- libs/web-components/slider/test/slider.test.ts
+++ libs/web-components/slider/test/slider.test.ts
@@ -1,10 +1,10 @@
-import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
-import { Slider } from '../src/slider.js';
+import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
 import '../src/slider.js';
+import { Slider } from '../src/slider.js';
 
 describe('Slider', () => {
   it('loads accessibly', async () => {
-    const el: Slider = await fixture(html`<fds-slider></fds-slider>`);
+    const el: Slider = await fixture(html`<fds-slider aria-label="accessible"></fds-slider>`);
 
     await elementUpdated(el);
     await expect(el).to.be.accessible();
