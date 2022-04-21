590c282a99615839295522b8b5b69aa0e510692c
frigui dorsaf <dorsafrigui@gmail.com>
2022-04-21 14:25:35 +0200

    fix unit tests

--- libs/web-components/chip/test/chip.test.ts
+++ libs/web-components/chip/test/chip.test.ts
@@ -4,7 +4,15 @@ import '../src/chip.js';
 
 describe('Chip', () => {
   it('loads accessibly', async () => {
-    const el: Chip = await fixture(html`<fds-chip></fds-chip>`);
+    const el: Chip = await fixture(html`<fds-chip label="Test"></fds-chip>`);
+
+    await elementUpdated(el);
+    await expect(el).to.be.accessible();
+  });
+
+
+  it('displays icons', async () => {
+    const el: Chip = await fixture(html`<fds-chip label="Test" icon="event" TrailingIcon="cancel"></fds-chip>`);
 
     await elementUpdated(el);
     await expect(el).to.be.accessible();
