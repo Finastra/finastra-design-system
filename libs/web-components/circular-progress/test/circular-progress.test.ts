c71a75160011cb50f5f4d7c12133b2c5d3067b30
pi5rr5 <sim.pierre91@gmail.com>
2022-05-13 11:30:53 +0300

    feat(#712) add aria-label

--- libs/web-components/circular-progress/test/circular-progress.test.ts
+++ libs/web-components/circular-progress/test/circular-progress.test.ts
@@ -1,10 +1,10 @@
-import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
-import { CircularProgress } from '../src/circular-progress.js';
+import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
 import '../src/circular-progress.js';
+import { CircularProgress } from '../src/circular-progress.js';
 
 describe('CircularProgress', () => {
   it('loads accessibly', async () => {
-    const el: CircularProgress = await fixture(html`<fds-circular-progress></fds-circular-progress>`);
+    const el: CircularProgress = await fixture(html`<fds-circular-progress aria-label="accessible"></fds-circular-progress>`);
 
     await elementUpdated(el);
     await expect(el).to.be.accessible();
--- libs/web-components/linear-progress/test/linear-progress.test.ts
+++ libs/web-components/linear-progress/test/linear-progress.test.ts
@@ -1,10 +1,10 @@
-import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
-import { LinearProgress } from '../src/linear-progress.js';
+import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
 import '../src/linear-progress.js';
+import { LinearProgress } from '../src/linear-progress.js';
 
 describe('LinearProgress', () => {
   it('loads accessibly', async () => {
-    const el: LinearProgress = await fixture(html`<fds-linear-progress></fds-linear-progress>`);
+    const el: LinearProgress = await fixture(html`<fds-linear-progress aria-label="accessible"></fds-linear-progress>`);
 
     await elementUpdated(el);
     await expect(el).to.be.accessible();
