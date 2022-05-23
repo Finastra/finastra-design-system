0a2a9fdfd6315e310e30b2f34583effa4a532f26
frigui dorsaf <dorsafrigui@gmail.com>
2022-05-23 17:03:32 +0200

    dispatch step click event

--- libs/web-components/stepper/src/base-stepper.ts
+++ libs/web-components/stepper/src/base-stepper.ts
@@ -21,17 +21,18 @@ export class BaseStepper extends LitElement {
   currentStepIndex = -1;
 
   renderIconAndLine(index: number) {
-    const startLineClass = { hidden: index === 0, current: index === (this.currentStepIndex + 1) && !this.steps[index-1].disabled, first: index === 0};
+    const startLineClass = { hidden: index === 0, current: index === (this.currentStepIndex + 1) && !this.steps[index - 1].disabled, first: index === 0 };
     const endLineClass = { hidden: index === this.steps.length - 1, last: index === this.steps.length - 1 };
     return html`
       <div class="line  start-line ${classMap(startLineClass)}"></div>
-      <div class="circle step-item-icon">
+      <div class="circle step-item-icon" @click="${() => this._onStepClick(index)}">
         ${index >= this.currentStepIndex
         ? index + 1
         : svg`<svg width="14" height="11" viewBox="0 0 14 11">
-<path d="M4.75012 8.12757L1.62262 5.00007L0.557617 6.05757L4.75012 10.2501L13.7501 1.25007L12.6926 0.192566L4.75012 8.12757Z"/>
-</svg>
-`}
+          <path
+            d="M4.75012 8.12757L1.62262 5.00007L0.557617 6.05757L4.75012 10.2501L13.7501 1.25007L12.6926 0.192566L4.75012 8.12757Z" />
+        </svg>
+        `}
       </div>
       <div class="line end-line ${classMap(endLineClass)}"></div>
     `;
@@ -39,18 +40,35 @@ export class BaseStepper extends LitElement {
 
   render() {
     return html`<div class="container">
-      ${this.steps.map(
-      (step, idx) =>
-        html`<div class="step-item ${idx < this.currentStepIndex? 'done' : ''} ${idx === this.currentStepIndex && !step.disabled? 'current' : ''} ${step.disabled && idx >= this.currentStepIndex ? 'disabled' : ''} ">
-            ${this.renderIconAndLine(idx)}
-            ${step.description
-            ? html`<div class="text-wrapper">
-                  <div class="step-item-label">${step.label}</div>
-                  <div class="step-item-description">${step.description}</div>
-                </div>`
-            : html`<div class="step-item-label">${step.label}</div>`}
-          </div>`
-    )}
-    </div>`;
+  ${this.steps.map(
+  (step, idx) =>
+    html`<div
+    class="step-item ${idx < this.currentStepIndex ? 'done' : ''} ${idx === this.currentStepIndex && !step.disabled ? 'current' : ''} ${step.disabled ? 'disabled' : ''} ">
+    ${this.renderIconAndLine(idx)}
+    ${step.description
+    ? html`<div class="text-wrapper">
+      <div class="step-item-label">${step.label}</div>
+      <div class="step-item-description">${step.description}</div>
+    </div>`
+    : html`<div class="step-item-label">${step.label}</div>`}
+  </div>`
+  )}
+</div>`;
+  }
+
+  _onStepClick(index: number) {
+    if (!this.steps[index]?.disabled) {
+      this.currentStepIndex = index;
+      this.dispatchEvent(
+        new CustomEvent('step-click', {
+          bubbles: true, composed: true, detail: {
+            value: `${index}`
+          }
+        })
+      )
+    }
+    else {
+      return;
+    }
   }
 }
