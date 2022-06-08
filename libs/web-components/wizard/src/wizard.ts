409d40982470f0c0cc51b2e30f0077003e9b4077
frigui dorsaf <dorsafrigui@gmail.com>
2022-06-08 17:31:56 +0200

    remove any and create a page interface

--- libs/web-components/wizard/src/wizard.ts
+++ libs/web-components/wizard/src/wizard.ts
@@ -10,11 +10,16 @@ export enum POSITION {
   left = 'left',
 }
 
+export interface Page {
+  label: string;
+  description?: string;
+  disabled?: Boolean;
+}
+
 /**
   * @cssprop {color} [--fds-stepper-bg=#fafafa] - Stepper background color
   * @attr {boolean} [stepperOnDark=false] - Stepper on dark
 */
-
 @customElement('fds-wizard')
 export class Wizard extends LitElement {
   static styles = styles;
@@ -42,7 +47,7 @@ export class Wizard extends LitElement {
   protected save = false;
   protected back = false;
   protected disabled: Boolean | null = null;
-  protected arrayPages: any[] = [];
+  protected arrayPages: Page[] = [];
   protected currentStepIndex = 0;
 
   constructor() {
@@ -102,13 +107,13 @@ export class Wizard extends LitElement {
   }
 
   onPagesSlotChanged() {
-    (this._pages[this.currentStepIndex] as any).setAttribute('current', true);
-    this._pages.forEach((page: any, index) => {
+    (this._pages[this.currentStepIndex]).setAttribute('current', 'true');
+    this._pages.forEach((page: HTMLElement, index: number) => {
       this.checkAttributes(page, index);
       this.arrayPages.push({
-        'label': page.getAttribute('title'),
-        'description': page.getAttribute('description'),
-        'disabled': this.disabled
+        'label': page.getAttribute('title') as string,
+        'description': page.getAttribute('description') as string,
+        'disabled': this.disabled as Boolean
       });
       this.disabled = null;
     });
@@ -131,7 +136,7 @@ export class Wizard extends LitElement {
   }
 
   checkNextStepDisabled(pages: Array<HTMLElement>, current: number) {
-    if ((pages[current] as any).hasAttribute('disabled')) {
+    if ((pages[current]).hasAttribute('disabled')) {
       this.stepper['currentStepIndex']++;
       this.currentStepIndex++;
       current++;
@@ -142,7 +147,7 @@ export class Wizard extends LitElement {
   }
 
   checkPreviousStepDisabled(pages: Array<HTMLElement>, current: number) {
-    if ((pages[current] as any).hasAttribute('disabled')) {
+    if ((pages[current]).hasAttribute('disabled')) {
       this.stepper['currentStepIndex']--;
       this.currentStepIndex--;
       current--;
@@ -159,14 +164,14 @@ export class Wizard extends LitElement {
   goToNextStep(pages: Array<HTMLElement>) {
     this.currentStepIndex = this.stepper['currentStepIndex'];
     this.stepper['currentStepIndex']++;
-    (pages[this.currentStepIndex] as any).removeAttribute("current");
+    (pages[this.currentStepIndex]).removeAttribute('current');
     this.currentStepIndex++;
   }
 
   goToPreviousStep(pages: Array<HTMLElement>) {
     this.currentStepIndex = this.stepper['currentStepIndex'];
     this.stepper['currentStepIndex']--;
-    (pages[this.currentStepIndex] as any).removeAttribute("current");
+    (pages[this.currentStepIndex]).removeAttribute('current');
     this.currentStepIndex--;
   }
 
@@ -185,9 +190,9 @@ export class Wizard extends LitElement {
   }
 
   updateCurrentPage(index: number) {
-    (this._pages[this.currentStepIndex] as any).removeAttribute("current");
+    (this._pages[this.currentStepIndex]).removeAttribute('current');
     this.currentStepIndex = index;
-    (this._pages[this.currentStepIndex] as any).setAttribute('current', true);
+    (this._pages[this.currentStepIndex]).setAttribute('current', 'true');
   }
 
   _handleNextClick() {
@@ -195,7 +200,7 @@ export class Wizard extends LitElement {
     if (this.currentStepIndex !== (this._pages.length - 1)) {
       this.goToNextStep(this._pages);
       this.checkNextStepDisabled(this._pages, this.currentStepIndex);
-      (this._pages[this.currentStepIndex] as any).setAttribute('current', true);
+      (this._pages[this.currentStepIndex]).setAttribute('current', 'true');
     }
     if (this.currentStepIndex + 1 === (this._pages.length)) {
       this.save = true;
@@ -213,7 +218,7 @@ export class Wizard extends LitElement {
       }
       this.goToPreviousStep(this._pages);
       this.checkPreviousStepDisabled(this._pages, this.currentStepIndex);
-      (this._pages[this.currentStepIndex] as any).setAttribute('current', true);
+      (this._pages[this.currentStepIndex]).setAttribute('current', 'true');
     }
     this.requestUpdate();
   }
