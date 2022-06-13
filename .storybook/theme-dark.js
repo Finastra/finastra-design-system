7eaed4f6ba90746cbc3cf601e9bc02214f7da607
pi5rr5 <sim.pierre91@gmail.com>
2022-06-13 20:33:20 +0200

    revert modification for dark theme

--- .storybook/theme-dark.js
+++ .storybook/theme-dark.js
@@ -5,6 +5,8 @@ export default create({
   base: 'dark',
   ...baseTheme,
 
+  brandImage: 'https://res.cloudinary.com/ffdc/image/upload/c_scale,w_350/v1634050263/finastra_logo_rgb_rev.png',
+
   colorPrimary: '#E453BF',
   colorSecondary: '#917EE0',
 
