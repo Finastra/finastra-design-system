668be438104fb1dc71b984935f51d09a022b4485
frigui dorsaf <dorsafrigui@gmail.com>
2022-06-16 13:20:20 +0200

    correct category

--- libs/web-components/wizard-page/stories/wizard-page.stories.ts
+++ libs/web-components/wizard-page/stories/wizard-page.stories.ts
@@ -6,7 +6,7 @@ import { html } from 'lit-html';
 import { argTypes, cssprops } from './custom-element.json';
 
 export default {
-  title: 'Components/WizardPage',
+  title: 'FORMS/WizardPage',
   component: 'fds-wizard-page',
   argTypes,
   parameters: {
