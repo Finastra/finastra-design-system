add46fea16fd23b448f2977e4d2f40935c0111dc
frigui dorsaf <dorsafrigui@gmail.com>
2022-06-30 18:18:47 +0200

    update storybook

--- libs/web-components/badge-container/stories/bagde-container.stories.ts
+++ libs/web-components/badge-container/stories/bagde-container.stories.ts
@@ -5,7 +5,7 @@ import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
 
 export default {
-  title: 'DATA DISPLAY/badgeContainer',
+  title: 'DATA DISPLAY/BadgeContainer',
   component: 'fds-badge-container',
   args: {
     name: 'World'
@@ -17,13 +17,39 @@ export default {
   },
   decorators: [
     (story) => html`${story()}<style>
-        /* Add you styles here */
+        fds-badge-container span {
+          font: var(--fds-body-1);
+          color: var(--fds-on-background);
+        }
       </style>`
   ]
 } as Meta;
 
-const Template: Story<badgeContainer> = ({ name = 'World' }) => {
-  return html`<fds-badge-container  .name=${name}></fds-badge-container>`;
+const Template: Story<badgeContainer> = ({ }) => {
+  return html`<fds-badge-container>
+    <span> Success</span>
+    <fds-badge value="1" color="success"></fds-badge>
+  </fds-badge-container>`;
+};
+
+const IconButtonTemplate: Story<badgeContainer> = ({}) => {
+  return html`<fds-badge-container>
+    <mwc-icon-button icon="bookmark_border"></mwc-icon-button>
+    <fds-badge value="1" color="success" position= "topRight"></fds-badge>
+  </fds-badge-container>`;
+};
+
+
+const ButtonTemplate: Story<badgeContainer> = ({}) => {
+  return html`<fds-badge-container>
+    <fds-button label="button" icon="notifications"></fds-button>
+    <fds-badge value="1" color="success" position="topRight"></fds-badge>
+  </fds-badge-container>`;
 };
 
 export const Default: Story<badgeContainer> = Template.bind({});
+
+export const WithIconButton: Story = IconButtonTemplate.bind({});
+
+export const WithButton: Story = ButtonTemplate.bind({});
+
--- libs/web-components/badge/stories/badge.stories.ts
+++ libs/web-components/badge/stories/badge.stories.ts
@@ -1,28 +1,33 @@
 const README = require('../README.md');
 import '@finastra/badge';
-import '@finastra/badge-container';
 import type { Badge } from '@finastra/badge';
+import '@finastra/badge-container';
 import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
 
 export default {
   title: 'DATA DISPLAY/Badge',
   component: 'fds-badge',
-  args: {
-    name: 'World'
-  },
   parameters: {
     docs: {
       description: { component: README }
     }
-  }
+  },
+  decorators: [
+    (story) => html`${story()}<style>
+      </style>`
+  ]
 } as Meta;
 
 const Template: Story<Badge> = ({value, color, position }) => {
-  return html`<fds-badge-container>
-    <span> Success</span>
+  return html`
     <fds-badge value=${value} color=${color} position=${position}></fds-badge>
-  </fds-badge-container>`;
+    `;
 };
 
+
 export const Default: Story<Badge> = Template.bind({});
+Default.args = {
+value: "1",
+color: "success"
+};
