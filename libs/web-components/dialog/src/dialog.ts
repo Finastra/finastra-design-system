9435cb1dc387bf1b519dd3d681ec8f3d3d5c7f5b
frigui dorsaf <dorsafrigui@gmail.com>
2022-07-06 18:52:41 +0200

    doc

--- libs/web-components/dialog/src/dialog.ts
+++ libs/web-components/dialog/src/dialog.ts
@@ -2,9 +2,19 @@ import { DialogBase } from '@material/mwc-dialog/mwc-dialog-base';
 import { customElement } from 'lit/decorators.js';
 import { styles } from './styles.css';
 /** 
-* @attr [heading=''] - Title.
-* @attr [open=false] - Opens.
-* @attr [hideActions=false] - Hide actions.
+* @cssprop [--fds-dialog-max-height=calc(100% - 32px)] - Sets dialog max height
+* @cssprop [--fds-dialog-max-width=560px]- Sets dialog max width
+* @cssprop [--fds-dialog-min-width=280px]- Sets dialog min width
+$ @cssprop [--fds-dialog-z-index=7] - Sets the z-index of the dialog and scrim
+* @cssprop {color} [--fds-dialog-scrim-color=rgba(0, 0, 0, 0.12)] - Color of the scrim
+* @slot primaryAction - Footer area containing the dialog's primary action button.
+* @slot secondaryAction - Footer area containing the dialog's secondary action button.
+* @attr [stacked=false] - Whether to stack the action buttons
+* @attr [scrimClickAction=""] - Setting this attribute to an empty string "" will prevent clicks outside the dialog from closing the dialog
+* @attr [escapeKeyAction=""] - Setting this attribute to an empty string "" will prevent the escape key from closing the dialog
+* @attr [heading=''] - Heading text of the dialog.
+* @attr [open=false] - Whether the dialog should open
+* @attr [hideActions=false] - Hides the actions footer of the dialog
 */
 @customElement('fds-dialog')
 export class Dialog extends DialogBase {
--- libs/web-components/dialog/stories/dialog.stories.ts
+++ libs/web-components/dialog/stories/dialog.stories.ts
@@ -3,33 +3,32 @@ import '@finastra/dialog';
 import type { Dialog } from '@finastra/dialog';
 import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
-import { argTypes } from "./custom-element.json";
-
+import { argTypes, cssprops } from "./custom-element.json";
 
 export default {
-  title: 'Components/Dialog',
+  title: 'POPOVER/Dialog',
   component: 'fds-dialog',
   argTypes,
   parameters: {
     docs: {
       description: { component: README }
-    }
+    },
+    cssprops
   },
   decorators: [
     (story) => html`${story()}
-      <script>
-        function myFunction(){
-          document.getElementById("myDropdown").show();
-        }
-      </script>
-      `
+<script>
+  function showDialog(){
+    document.getElementById("dialog").show();
+  }
+</script>`
   ]
 } as Meta;
 
-const Template: Story<Dialog> = ({ open, heading, hideActions}) => {
+const Template: Story<Dialog> = ({ open, heading, hideActions, stacked, scrimClickAction, escapeKeyAction}) => {
   return html`
-<fds-button label="open" onClick="myFunction()"></fds-button>
-<fds-dialog id="myDropdown" heading=${heading} .open=${open} ?hideActions=${hideActions}>
+<fds-button label="Open" onClick="showDialog()"></fds-button>
+<fds-dialog id="dialog" heading=${heading} ?open=${open} ?hideActions=${hideActions} ?stacked=${stacked} ?scrimClickAction=${scrimClickAction} ?escapeKeyAction=${escapeKeyAction}>
   <span class="body">Body 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </span>
   <fds-button
       secondary
@@ -39,15 +38,14 @@ const Template: Story<Dialog> = ({ open, heading, hideActions}) => {
   </fds-button>
   <fds-text-button
       label="No"
-      slot="secondaryAction"
-      dialogAction="cancel">
+      slot="secondaryAction",
+      dialogAction="cancel"
+  >
   </fds-text-button>
-  </fds-dialog>`;
+</fds-dialog>`;
 };
 
 export const Default: Story<Dialog> = Template.bind({});
 Default.args = {
-  heading: "title"
+  heading: "Title"
 };
-
-
