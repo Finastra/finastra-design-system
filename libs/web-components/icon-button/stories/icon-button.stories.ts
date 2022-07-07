1f97e75ab3cc47689c276cc28216146b4345d293
Talbot, Thomas <thomas.talbot@finastra.com>
2022-07-07 15:26:14 +0200

    doc: add Figma content in text field and icon button

--- libs/web-components/icon-button/stories/icon-button.stories.ts
+++ libs/web-components/icon-button/stories/icon-button.stories.ts
@@ -13,6 +13,10 @@ export default {
     docs: {
       description: { component: README }
     },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/E1Mb1556RT3HbAUVu2Q0LV/Finastra-design-system?node-id=107363%3A34403'
+    },
     cssprops
   }
 } as Meta;
