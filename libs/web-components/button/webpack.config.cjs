89b92a1ba469d4df89baa67f57b7e7a7585b1635
David Boclé <bcldvd@users.noreply.github.com>
2022-07-26 13:24:13 +0200

    feat(wc:button): Output a single bundle containing everything (#806)
    * feat(wc:button): Output a single bundle containing everything
    
    * remove -full from name of output bundle

---
+++ libs/web-components/button/webpack.config.cjs
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'fds-button.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+module.exports = config;
