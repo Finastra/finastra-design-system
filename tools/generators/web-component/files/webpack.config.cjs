92679dc428a48685c48d1724234ae6515bb8627a
David Boclé <david.bocle@finastra.com>
2022-08-19 11:39:12 +0200

    fix

--- tools/generators/web-component/files/webpack.config.cjs
+++ tools/generators/web-component/files/webpack.config.cjs
@@ -1,7 +1,7 @@
 const path = require('path');
 const config = require('../base-webpack.config.cjs');
 
-config.entry = path.resolve(__dirname, './src/index.ts'),
+config.entry = path.resolve(__dirname, './src/<%= fileName %>.ts'),
 config.output = {
     filename: 'fds-<%= fileName %>.js',
     path: path.resolve(__dirname, 'dist'),
