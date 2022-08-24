d26a9e01137e35a365ec7c63ee24a4dab3175eb4
David Boclé <david.bocle@finastra.com>
2022-08-24 11:32:03 +0200

    fix(wc:divider): Story from mdx to ts
    Fixes #746

--- libs/web-components/divider/src/divider.ts
+++ libs/web-components/divider/src/divider.ts
@@ -1,8 +1,13 @@
-import { customElement, property } from 'lit/decorators.js';
 import { LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
 import { styles } from './styles.css';
-
+/**
+ * @cssprop {color} [--fds-primary=#694ED6] - TextField color
+ * @cssprop {text} [--fds-divider-inset-margin=80px] - Border radius of the outline.
+ * @attr [vertical=false] - Displays the divider vertically instead of horizontically
+ * @attr {"both"|"left"|"right"|"undefined"} [inset=undefined] - Type of inset
+ */
 @customElement('fds-divider')
 export class Divider extends LitElement {
   static styles = styles;
