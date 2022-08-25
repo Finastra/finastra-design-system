51556a130679c295438e718f5d68ea3ea28ef588
frigui dorsaf <dorsafrigui@gmail.com>
2022-08-25 09:20:08 +0200

    flatten the tree

--- libs/web-components/filter-tree/src/filter-tree.ts
+++ libs/web-components/filter-tree/src/filter-tree.ts
@@ -6,9 +6,14 @@ import { styles } from './styles.css';
 export interface TreeNode {
   label: string;
   children?: TreeNode[];
-  parent?: TreeNode;
   isSelected?: boolean;
   level?: number
+}
+
+export interface TranformedTreeNode{
+  item: string;
+  level: number;
+  expandable: boolean;
 }
 
 @customElement('fds-filter-tree')
@@ -21,6 +26,7 @@ export class FilterTree extends LitElement {
     super();
   }
 
+  private itemsTransformed: TranformedTreeNode[] = [];
   private _items: TreeNode[] = [];
   @property({ attribute: false })
   public get items(): TreeNode[] {
@@ -31,6 +37,7 @@ export class FilterTree extends LitElement {
   }
   
   render() {
+    this.itemsTransformed=this.transformer(this.items,0,[]);
     return html`
       ${this.renderChildren(this.items)}
     `;
@@ -41,7 +48,7 @@ export class FilterTree extends LitElement {
       <ul id="list" multi>
         ${items.map((item) => {
         return html`
-          <fds-check-list-item  left @request-selected="${(event) => this.onRequestSelected(item, event)}"> ${item.label} </fds-check-list-item>
+          <fds-check-list-item  left @request-selected="${(event) => this.onRequestSelected(item, event)}">${item.label} </fds-check-list-item>
           ${item.children ? html` ${this.renderChildren(item.children)} `: ''}
         `
       })}
@@ -51,6 +58,8 @@ export class FilterTree extends LitElement {
   onRequestSelected(item: TreeNode, event) {
     const eventType = event.detail.source;
     if(eventType === 'interaction'){
+      this.checkAllParentsSelection(this.getItemFromTransformedData(item));
+      this.getParentNode(this.getItemFromTransformedData(item));
       if(this.getNodeByLabel(item.label).getAttribute('selected') == null) {
         if(item.children){
           this.selectChildren(item.children);
@@ -82,7 +91,7 @@ export class FilterTree extends LitElement {
     })
   }
 
-getNodeByLabel(index) {
+ getNodeByLabel(index) {
   let selected ;
   const itemSelector='fds-check-list-item';
   const itemEl = this.renderRoot?.querySelectorAll<HTMLSlotElement>(itemSelector);
@@ -93,6 +102,65 @@ getNodeByLabel(index) {
     })
   return selected;
  }
+
+
+  getLevel(node) {
+    return node.level;
+  }
+
+transformer(items: TreeNode[], level = 0, output:TranformedTreeNode[]) {
+  items.forEach(element => {
+    element.level = level;
+    let i = {item: element.label, level: element.level, expandable: !!element.children?.length}
+    output.push(i as TranformedTreeNode);
+
+    if(element.children)
+    this.transformer(element.children , level+1 ,output);
+  });
+return output
+}
+
+getParentNode(node: TranformedTreeNode): TranformedTreeNode | null {
+  const currentLevel = node.level;
+
+  if (currentLevel < 1) {
+    return null;
+  }
+
+  const startIndex =  this.itemsTransformed.indexOf(node);
+
+  for (let i = startIndex; i >= 0; i--) {
+    const currentNode = this.itemsTransformed[i];
+    if (this.getLevel(currentNode) < currentLevel) {
+      return currentNode;
+    }
+  }
+  return null;
+}
+
+getItemFromTransformedData(item: TreeNode) {
+  let selected ;
+  this.itemsTransformed.forEach((node) => {
+    if(node.item == item.label) {
+      selected=node;
+    }
+})
+return selected;
+}
+
+checkAllParentsSelection(node: TranformedTreeNode): void {
+  let parent: TranformedTreeNode | null = this.getParentNode(node);
+  while (parent) {
+    console.log(parent);
+    //this.checkRootNodeSelection(parent);
+    parent = this.getParentNode(parent);
+  }
+}
+
+// checkRootNodeSelection(node: TranformedTreeNode): void {
+//   if(this.getNodeByLabel(node.item).getAttribute('selected') == null) {
+//   }
+// }
 }
 
 declare global {
