69e79fb5d8710dcb1954520f678479ca38769f8f
Talbot, Thomas <thomas.talbot@finastra.com>
2022-10-26 18:34:55 +0200

    remove irrelevant button test

--- libs/web-components/button/test/button.test.ts
+++ libs/web-components/button/test/button.test.ts
@@ -1,15 +1,7 @@
 import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
-import '../src/outlined-button.js';
 import { OutlinedButton } from '../src/outlined-button.js';
 
 describe('Button', () => {
-  it('should be able to set outline', async () => {
-    const el: OutlinedButton = await fixture(html`<fds-outlined-button label="Outlined button"></fds-outlined-button><br />`);
-
-    await elementUpdated(el);
-    await expect(shadowRoot(el).querySelector('mwc-button')?.hasAttribute('outlined')).to.equal(true);
-  });
-
   describe(' - Accessibility', () => {
     it('loads accessibly', async () => {
       const el: OutlinedButton = await fixture(html`<fds-outlined-button></fds-outlined-button>`);
@@ -19,7 +11,3 @@ describe('Button', () => {
     });
   });
 });
-
-function shadowRoot(el: Element) {
-  return el.shadowRoot ? el.shadowRoot : el;
-}
