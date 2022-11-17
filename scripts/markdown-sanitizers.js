465be8b73292b269b7dfd2ec7f48a4548ec8c882
David Boclé <david.bocle@finastra.com>
2022-11-17 12:09:26 +0100

    inject github readme link in place of storybook one

--- scripts/markdown-sanitizers.js
+++ scripts/markdown-sanitizers.js
@@ -1,16 +1,32 @@
+const { matches } = require("lodash-es");
+
 function wcaApiDocRemover(markdown) {
   const regex = /(<!-- DOC.* -->)(.*)(<!-- \/DOC.* -->)/gms;
   return markdown.replace(regex, '');
 };
 
-function storybookButton(markdown) {
+function storybookButton(markdown, replace) {
   const regex = /\[\!\[Storybook\](.*)/gm;
-  return markdown.replace(regex, '');
+  return markdown.replace(regex, replace);
 };
 
+function githubReadme(link) {
+  const GITHUB_BADGE = 'https://img.shields.io/badge/GitHub-README-000?style=for-the-badge&logo=github&logoColor=white';
+  return `[![${GITHUB_BADGE}](${GITHUB_BADGE})](${link})`;
+}
+
+function getLinkToReadme(componentPath) {
+  return `https://github.com/Finastra/finastra-design-system/blob/master/libs/web-components/${componentPath}/README.md`;
+}
+
+function getPathToComponent(markdown) {
+  let regex = /https:\/\/www\.npmjs\.com\/package\/@finastra\/(.*)\)/gm;
+  const matches = regex.exec(markdown);
+  return matches.length > 0 ? matches[1] : '';
+}
+
 function allSanitizers(markdown) {
-  return markdown;
-  return storybookButton(wcaApiDocRemover(markdown));
+  return storybookButton(wcaApiDocRemover(markdown), githubReadme(getLinkToReadme(getPathToComponent(markdown))));
 }
 
 module.exports = {
