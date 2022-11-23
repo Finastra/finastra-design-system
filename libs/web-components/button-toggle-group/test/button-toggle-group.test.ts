c62f64872940c71763f3d4baa09ce4d07d765a88
Talbot, Thomas <thomas.talbot@finastra.com>
2022-11-23 16:51:32 +0100

    feat(toggle-group): update unit tests

--- libs/web-components/button-toggle-group/test/button-toggle-group.test.ts
+++ libs/web-components/button-toggle-group/test/button-toggle-group.test.ts
@@ -1,10 +1,14 @@
-import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
-import { ButtonToggleGroup } from '../src/button-toggle-group.js';
+import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
 import '../src/button-toggle-group.js';
+import { ButtonToggleGroup } from '../src/button-toggle-group.js';
 
 describe('ButtonToggleGroup', () => {
   it('loads accessibly', async () => {
-    const el: ButtonToggleGroup = await fixture(html`<fds-button-toggle-group></fds-button-toggle-group>`);
+    const el: ButtonToggleGroup = await fixture(html`
+      <fds-button-toggle-group>
+        <fds-button-toggle icon="favorite"></fds-button-toggle>
+      </fds-button-toggle-group>
+    `);
 
     await elementUpdated(el);
     await expect(el).to.be.accessible();
