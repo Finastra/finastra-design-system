aa7e4f8c92bd5931400bf5726d4f63864e73bb70
David Boclé <david.bocle@finastra.com>
2022-12-13 15:37:24 +0100

    formating

--- scripts/pre-angular-release.js
+++ scripts/pre-angular-release.js
@@ -1,30 +1,26 @@
 const { exec } = require('child_process');
 const chalk = require('chalk');
 
-const forbiddenBranches = [
-  'master'
-];
+const forbiddenBranches = ['master'];
 
 const main = async () => {
   const branchName = await getGitBranch();
   if (forbiddenBranches.includes(branchName)) {
-    console.error(`✋ Cannot launch release on branch ${chalk.blue(branchName)}. Please create a release branch beforehand !`)
+    console.error(`✋ Cannot launch release on branch ${chalk.blue(branchName)}. Please create a release branch beforehand !`);
     console.log(`
     git checkout -b release/angular-${chalk.red('VERSION')}
-    `)
+    `);
     process.exit(1);
   }
-}
+};
 
 main();
 
-function getGitBranch(){
+function getGitBranch() {
   return new Promise((resolve, reject) => {
     exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
-        if (err)
-            reject(`getGitBranch Error: ${err}`);
-        else if (typeof stdout === 'string')
-            resolve(stdout.trim());
+      if (err) reject(`getGitBranch Error: ${err}`);
+      else if (typeof stdout === 'string') resolve(stdout.trim());
     });
   });
 }
