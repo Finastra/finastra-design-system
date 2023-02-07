f9104ad0e2c48a18087986aa39ac95814e845eb3
frigui dorsaf <dorsafrigui@gmail.com>
2023-02-07 16:02:37 +0100

    add margin-top to floating label

--- libs/web-components/textarea/src/base-textarea.ts
+++ libs/web-components/textarea/src/base-textarea.ts
@@ -4,7 +4,6 @@ import { property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 
 export class BaseTextarea extends TextAreaBase {
-  @property({ type: Boolean }) showActionButton = false;
   @property({ type: Boolean }) dense = false;
   @property({ type: Boolean }) labelInside = false;
 
