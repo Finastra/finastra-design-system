6aa6eea2ca8356eeca67551ec71a1e46c99199f2
Talbot, Thomas <thomas.talbot@finastra.com>
2023-03-07 15:27:35 +0100

    format story

--- packages/fds-components-web/credit-card/stories/credit-card.stories.ts
+++ packages/fds-components-web/credit-card/stories/credit-card.stories.ts
@@ -2,7 +2,7 @@ const README = require('../README.md');
 import '@finastra/credit-card';
 import type { CreditCard } from '@finastra/credit-card';
 import { Meta, Story } from '@storybook/web-components';
-import { html } from 'lit-html';
+import { html } from 'lit';
 import { allSanitizers } from '../../../../scripts/markdown-sanitizers';
 import { argTypes, cssprops } from './sb-generated/fds-credit-card.json';
 
@@ -14,12 +14,21 @@ export default {
     docs: {
       description: { component: allSanitizers(README) }
     },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/6BBm9XAsO9lQZQ4xBM4cGr/Patterns-%26-apps?node-id=33364%3A25330&t=oHjPoRuZpC5vEPc7-0'
+    }
   },
   cssprops
 } as Meta;
 
-const Template: Story<CreditCard> = ({ name = '', lastDigits, hiddenDigits, date }) => {
-  return html`<fds-credit-card  .name=${name} .last-digits=${lastDigits} .hidden-digits=${hiddenDigits} .date=${date}></fds-credit-card>`;
+const Template: Story<CreditCard> = ({ name, lastDigits, hiddenDigits, date }) => {
+  return html`<fds-credit-card
+    name=${name}
+    last-digits=${lastDigits}
+    hidden-digits=${hiddenDigits}
+    date=${date}
+  ></fds-credit-card>`;
 };
 
 export const Default: Story<CreditCard> = Template.bind({});
